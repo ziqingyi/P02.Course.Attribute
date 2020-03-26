a67cd8cedaac402f5abb1a7d39d5c52bc238bd47
Adrian Sun <adrian_sun@livingstone.com.au>
2020-03-26 16:52:12 +1100

    get remark

--- EnumExtend/RemarkExtend.cs
+++ EnumExtend/RemarkExtend.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,21 @@ namespace P02.Course.Attribute.EnumExtend
 {
     public static class RemarkExtend
     {
+        public static string GetRemark(this Enum enumValue)
+        {
+            Type type = enumValue.GetType();
+            FieldInfo field = type.GetField(enumValue.ToString());
+            if(field.IsDefined( typeof(RemarkAttribute), true) )
+            {
+                RemarkAttribute remarkAttribute =(RemarkAttribute)field.GetCustomAttributes(typeof(RemarkAttribute)));
+                return remarkAttribute._Remark;
+            }
+            else
+            {
+                return enumValue.ToString();
+            }
 
-
+        }
 
 
 
