e48939363d8ebabfe7f6f9144b3a5259d47fe77d
adrian sun <ziqingyi@qq.com>
2020-03-26 21:21:52 +1100

    attribute extend

---
+++ EnumExtend/AttributeExtend.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02.Course.Attribute.EnumExtend
+{
+    public static class AttributeExtend
+    {
+        public static string GetRemark(this Enum value)
+        {
+            Type type = value.GetType();
+            var field = type.GetField(value.ToString());
+            if (field.IsDefined(typeof(RemarkAttribute), true))
+            {
+                RemarkAttribute attribute = (RemarkAttribute)field.GetCustomAttribute(typeof(RemarkAttribute), true);
+                return attribute.Remark;
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+    }
+}
