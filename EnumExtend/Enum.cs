498d0e547fb0673e5280b1ddb80179836fc3f496
Adrian Sun <adrian_sun@livingstone.com.au>
2020-03-26 16:58:24 +1100

    remark user state

--- EnumExtend/Enum.cs
+++ EnumExtend/Enum.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace P02.Course.Attribute.EnumExtend
 {
+    
     public enum UserState
     {
         //normal state user
+        [Remark("Normal")]
         Normal=0,
-        
+
+        [Remark("Freeze")]
         Frozen =1,
+
+        [Remark("Delete")]
         Deleted = 2 
 
     }
