088f5131f240df433b5198e985d0314e1a8a42ae
Adrian Sun <adrian_sun@livingstone.com.au>
2020-04-03 08:28:40 +1100

    winfrom

---
+++ P03.Course.winForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P03.Course.winForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("log in");
+        }
+
+        private void btnPay_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("pay");
+        }
+    }
+}
