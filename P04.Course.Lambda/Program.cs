1ab3e3c3c837e74129f956c441b63db446db4ccd
adrian sun <ziqingyi@qq.com>
2020-04-05 20:44:48 +1000

    anonymous class, var

--- P04.Course.Lambda/Program.cs
+++ P04.Course.Lambda/Program.cs
@@ -10,6 +10,70 @@ namespace P04.Course.Lambda
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Lambda and linq");
+            //{
+            //    Console.WriteLine("***************Lambda*****************");
+            //    LambdaShow lambdaShow = new LambdaShow();
+            //    lambdaShow.Show();
+            //}
+
+            #region 3.0出了个匿名类 var
+
+            {
+                Console.WriteLine("****************anonymous class***************");
+                Student student = new Student()
+                {
+                    Id = 1,
+                    Name = "anonymous student",
+                    Age = 25,
+                    ClassId = 2
+                };
+                student.Study();
+
+                //.net 3.0 strong type language need to determine the type when compile
+                object mo = new
+                {
+                    Id = 1,
+                    Name = "anonymous student",
+                    Age = 25,
+                    ClassId = 2
+                    //teacher = "tom"
+                };
+                     // Console.WriteLine(mo.Id); wrong 
+                     var vv = new
+                     {
+                         Id = 1,
+                         Name = "anonymous student",
+                         Age = 25,
+                         ClassId = 2
+                     };
+                     //local variable, `a , anonymous class. 
+                     //vv.Id = 2; //read only, cannot change after creation. 
+                     var tt = 1; //compiler will define the type of tt based on first assigned value. 
+                     //var tti; //wrong  so it must have a value. 
+
+
+                //.net 4.0  dynamic can avoid compiler. 
+                dynamic dModel = new
+                {
+                    Id = 1,
+                    Name = "anonymous student",
+                    Age = 25,
+                    ClassId = 2
+                };
+                //it doesn't have Study() method. 
+                Console.WriteLine(dModel.id+ dModel.Name +" " + dModel.ClassId);
+                
+
+                //.net 3.0 extend method. 
+
+
+
+            }
+
+            #endregion
+
+
         }
     }
 }
--- P04.Course.Lambda/Student.cs
+++ P04.Course.Lambda/Student.cs
@@ -8,5 +8,30 @@ namespace P04.Course.Lambda
 {
     public class Student
     {
+        public int Id { get; set; }
+        public int ClassId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Study()
+        {
+            Console.WriteLine("{0} {1} is studying .net ", this.Id, this.Name);
+        }
+
+        public void StudyHard()
+        {
+            Console.WriteLine("{0} {1} is studying .net, study hard work hard ", this.Id, this.Name);
+        }
+
+        public void StudyPractise()
+        {
+            Console.WriteLine("{0} {1} is practice.......", this.Id, this.Name);
+        }
+    }
+
+    public class Class
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; }
     }
 }
