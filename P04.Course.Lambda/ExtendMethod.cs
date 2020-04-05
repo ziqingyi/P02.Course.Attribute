7037b7676581888fb857e2e071aef9ec1cec8b6d
adrian sun <ziqingyi@qq.com>
2020-04-05 21:09:57 +1000

    extend method

--- P04.Course.Lambda/ExtendMethod.cs
+++ P04.Course.Lambda/ExtendMethod.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace P04.Course.Lambda
 {
-    public class ExtendMethod
+    //Extend method: static method in static class, 
+    public static class ExtendMethod
     {
+        //if first param has this, stu.StudyPractise() can be used. //
+        public static void StudyPractise(this Student stu)
+        {
+            Console.WriteLine("{0} {1} is studying and practice.......extend method", stu.Id, stu.Name);
+        }
+
+        public static int ToInt(this int? i)
+        {
+            return i ?? 0;
+        }
+
+        public static string ToLength(this string text, int length = 15)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return "";
+            }
+            else if (text.Length > length)
+            {
+                return $"{text.Substring(0, length)} .....";
+            }
+            else
+            {
+                return text;
+            }
+        }
+
     }
 }
--- P04.Course.Lambda/Program.cs
+++ P04.Course.Lambda/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -62,12 +63,25 @@ namespace P04.Course.Lambda
                     ClassId = 2
                 };
                 //it doesn't have Study() method. 
-                Console.WriteLine(dModel.id+ dModel.Name +" " + dModel.ClassId);
+                Console.WriteLine(dModel.Id+ dModel.Name +" " + dModel.ClassId);
                 
 
                 //.net 3.0 extend method. 
+                   // 1 can be used in third party class, to add some method.
+                   // 2 if the class already have same name class, it will call the class's method. 
+                   // 3 So if the class is changed, it will has issue. 
+                   // 4 used for component in development, .netcore. the class is defined by min requirement.
+                   //   if you want to add more method, you can use extend method. 
+                   //    contex.Response.WriteAsync.   // middleware builder  use when 
+                   // 5 extend method which normally used. 
+                ExtendMethod.StudyPractise(student);//normal way 
+                student.StudyPractise();//because parameter has this
 
+                int? i = 1;//test for 5 
+                int ik = ExtendMethod.ToInt(i); //i.ToInt();
+                ik = i.ToInt();
 
+                string longtext = "adfakfkajflwjerjwqofjeqljldkfa".ToLength(20);//notice the 2nd param
 
             }
 
