370b76d262395b562c99816383a76da5d16db782
Adrian Sun <adrian_sun@livingstone.com.au>
2020-04-21 15:02:37 +1000

    test generic cache

---
+++ P01.Course.Generic/Extend/GenericCacheTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P01.Course.Generic.Extend
+{
+    public class GenericCacheTest
+    {
+        public static void Show()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(GenericCache<int>.GetCache());
+                Thread.Sleep(10);
+                Console.WriteLine(GenericCache<long>.GetCache());
+                Thread.Sleep(10);
+                Console.WriteLine(GenericCache<DateTime>.GetCache());
+                Thread.Sleep(10);
+                Console.WriteLine(GenericCache<string>.GetCache());
+                Thread.Sleep(10);
+                Console.WriteLine(GenericCache<GenericCacheTest>.GetCache());
+                Thread.Sleep(10);
+            }
+
+
+
+        }
+
+
+    }
+
+    class GenericCache<T>
+    {
+        private static string _type="";
+        static GenericCache()
+        {
+            Console.WriteLine("This is Generic Cache");
+            _type = string.Format("{0}_{1}", typeof(T).FullName, DateTime.Now);
+        }
+
+        public static string GetCache()
+        {
+            return _type;
+        }
+        
+
+
+    }
+
+
+}
---
+++ P01.Course.Generic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P01.Course.Generic.Extend;
+
+namespace P01.Course.Generic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---------test generic cache-----------");
+            GenericCacheTest.Show();
+
+            Console.WriteLine("--------test end--------------");
+
+
+
+        }
+}
+}
