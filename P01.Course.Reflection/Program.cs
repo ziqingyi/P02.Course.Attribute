f9b25026a187d83f150371e26c23d55c7f9debc7
Adrian Sun <adrian_sun@livingstone.com.au>
2020-04-23 12:46:28 +1000

    update reference and load dll

--- P01.Course.Reflection/Program.cs
+++ P01.Course.Reflection/Program.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using P01.Course.DB.MySql;
+
+
 
 namespace P01.Course.Reflection
 {
@@ -19,10 +22,31 @@ namespace P01.Course.Reflection
                     Console.WriteLine("---------common--------------");
                     IDBHelper iDbHelper = new MySqlHelper();
                     iDbHelper.Query();
+                }
+                {
+                   Assembly a1 =   Assembly.Load("P01.Course.DB.MySql");//just dll name, find dll first then exe
+                   Assembly a2 = Assembly.LoadFile(
+                       @"C:\Users\adrian_sun\Source\Repos\ziqingyi\P02.Course.Attribute\P01.Course.Reflection\bin\Debug\P01.Course.DB.MySql.dll");
+                   // full path
+
+                   // load from current folder 
+                   Assembly a3 = Assembly.LoadFrom("P01.Course.DB.SqlServerHelper.dll"); 
+                   Assembly a4 = Assembly.LoadFrom(
+                       @"C:\Users\adrian_sun\Source\Repos\ziqingyi\P02.Course.Attribute\P01.Course.Reflection\bin\Debug\P01.Course.DB.SqlServerHelper.dll");
+                   foreach (var type in a1.GetTypes())
+                   {
+                       Console.WriteLine(type.Name);
+                       foreach (var method in type.GetMethods())
+                       {
+                           Console.WriteLine(method.Name);
+                       }
+                   }
+
+
+
 
 
                 }
-
 
 
             }
