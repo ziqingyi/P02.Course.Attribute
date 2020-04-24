c91752ba753de1cdf10c0f11cccd3ee74c451f91
Adrian Sun <adrian_sun@livingstone.com.au>
2020-04-24 17:35:06 +1000

    update

--- P01.Course.Reflection/Program.cs
+++ P01.Course.Reflection/Program.cs
@@ -17,7 +17,10 @@ namespace P01.Course.Reflection
         {
             try
             {
-                //Console.WriteLine("------------reflection--------------------");
+                //Console.WriteLine("------------reflection------read the metadata of the dll or exe--------------");
+                // reflection is to read the metadata of the dll or exe
+                // pdb program debug base : used for debug 
+                //reference is to copy the project when debug. but you need to copy all project referred by the project. 
                 //{
                 //    Console.WriteLine("---------common--------------");
                 //    IDBHelper iDbHelper = new MySqlHelper();
@@ -37,6 +40,7 @@ namespace P01.Course.Reflection
                 //   foreach (var type in a1.GetTypes())
                 //   {
                 //       Console.WriteLine(type.Name);
+                //       type.IsGenericType()// check whether it is generic type
                 //       foreach (var method in type.GetMethods())
                 //       {
                 //           Console.WriteLine(method.Name);
@@ -74,13 +78,22 @@ namespace P01.Course.Reflection
                 //    Singleton s2 = Singleton.GetInstance();
                 //    Console.WriteLine(object.ReferenceEquals(s1, s2));
                 //}
-                {
-                    Console.WriteLine("------reflection to use private constructor-----------------");
-                    Assembly a1 = Assembly.Load("P01.Course.DB.SqlServer");
-                    Type t = a1.GetType("P01.Course.DB.SqlServer.Singleton");
-                    Singleton s1 =  Activator.CreateInstance(t,true) as Singleton;//will call two times of private constructor
+                //{
+                //    Console.WriteLine("------reflection to use private constructor-----------------");
+                //    Assembly a1 = Assembly.Load("P01.Course.DB.SqlServer");
+                //    Type t = a1.GetType("P01.Course.DB.SqlServer.Singleton");
+                //    Singleton s1 =  Activator.CreateInstance(t,true) as Singleton;//will call two times of private constructor
 
-                    Singleton s2 = Activator.CreateInstance(t, true) as Singleton;// call private constructor once 
+                //    Singleton s2 = Activator.CreateInstance(t, true) as Singleton;// call private constructor once 
+                //}
+                {
+                    Console.WriteLine("------------reflection and generic--------------------------");
+                    Assembly a1 = Assembly.Load("P01.Course.DB.SqlServer");
+                    Type t = a1.GetType("P01.Course.DB.SqlServer.GenericClass`3");
+                    //must notice `3
+                    Type typeMake = t.MakeGenericType(new Type[]{typeof(String), typeof(int),typeof(DateTime)});
+                    // make generic type first, then you can create object.
+                    object oGeneric = Activator.CreateInstance(typeMake);
 
                 }
 
