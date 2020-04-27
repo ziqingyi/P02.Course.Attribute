5b5d1645ba7747aff4cbec84707f85bdb6ca121b
adrian sun <ziqingyi@qq.com>
2020-04-27 21:34:11 +1000

    reflection with sql , details in your homework

--- P01.Course.DB.SqlServer/SqlServerHelper.cs
+++ P01.Course.DB.SqlServer/SqlServerHelper.cs
@@ -1,14 +1,22 @@
 ﻿using P01.Course.DB.Interface;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using P01.Course.Model;
 
 namespace P01.Course.DB.SqlServer
 {
     public class SqlServerHelper: IDBHelper
     {
+        private static string ConnectionStringCustomers =
+            ConfigurationManager.ConnectionStrings["Customers"].ConnectionString;
+
+
         public SqlServerHelper()
         {
             Console.WriteLine("{0} is being initialized ", this.GetType().Name);
@@ -17,5 +25,25 @@ namespace P01.Course.DB.SqlServer
         {
             Console.WriteLine("{0}.Query is being called", this.GetType().Name);
         }
+
+        public Company Find(int id)
+        {
+            string sql = "select * from atesCompany where id = " + id;
+            using (SqlConnection conn = new SqlConnection(ConnectionStringCustomers))
+            {
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+
+                var reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    Console.WriteLine(reader[1]);
+
+                }
+                return new Company();
+                
+            }
+        }
+
     }
 }
