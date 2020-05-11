c0a4493e5f0e7048a076e34cd757d1f4e07ea18e
adrian sun <ziqingyi@qq.com>
2020-05-11 20:38:01 +1000

    Sql Operator

--- P02.Course.ExpressionSty/DBExtend/SqlOperator.cs
+++ P02.Course.ExpressionSty/DBExtend/SqlOperator.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace P02.Course.ExpressionSty.DBExtend
 {
-    public class SqlOperator
+    internal static class SqlOperator
     {
+        internal static string ToSqlOperator(this ExpressionType type)
+        {
+            switch (type)
+            {
+                case (ExpressionType.AndAlso):
+                case (ExpressionType.And):
+                    return "AND";
+                case (ExpressionType.OrElse):
+                case (ExpressionType.Or):
+                    return "OR";
+                case (ExpressionType.Not):
+                    return "NOT";
+                case (ExpressionType.NotEqual):
+                    return "<>";
+                case ExpressionType.GreaterThan:
+                    return ">";
+                case ExpressionType.GreaterThanOrEqual:
+                    return ">=";
+                case ExpressionType.LessThan:
+                    return "<";
+                case ExpressionType.LessThanOrEqual:
+                    return "=";
+                default:
+                    throw new Exception("not in the sql operator list...");
+            }
+
+
+
+
+
+
+        }
+
+
+
     }
 }
