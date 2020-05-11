2e14ac585b63ac3987a6faa5b0918e15af8f37b9
adrian sun <ziqingyi@qq.com>
2020-05-11 20:50:59 +1000

    expression visitor

--- P02.Course.ExpressionSty/Visitor/ConditionBuilderVisitor.cs
+++ P02.Course.ExpressionSty/Visitor/ConditionBuilderVisitor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using P02.Course.ExpressionSty.DBExtend;
 
 namespace P02.Course.ExpressionSty.Visitor
 {
@@ -20,27 +21,65 @@ namespace P02.Course.ExpressionSty.Visitor
 
         protected override Expression VisitBinary(BinaryExpression node)
         {
-            if(node == null)
+            if (node == null)
+            {
                 throw new ArgumentNullException("BinaryExpression");
+            }
             this._StringStack.Push(")");
             base.Visit(node.Right);
             this._StringStack.Push( " " + node.NodeType.ToSqlOperator() + " ");
             base.Visit(node.Left);
             this._StringStack.Push("(");
-
             return node;
         }
 
         protected override Expression VisitMember(MemberExpression node)
         {
             if (node == null)
+            {
                 throw new ArgumentNullException("MemberExpression");
+            }
             this._StringStack.Push(" [" + node.Member.Name + "] ");
             return node;
         }
 
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException("ConstantExpression");
+            }
+            this._StringStack.Push(" '"+node.Value+"' ");
+            return node;
+        }
 
+        protected override Expression VisitMethodCall(MethodCallExpression m)
+        {
+            if (m == null)
+            {
+                throw new ArgumentNullException("MethodCallExpression");
+            }
 
+            string format;
+            switch (m.Method.Name)
+            {
+                case "StartsWith":
+                    format = "({0} LIKE {1}+'%')";
+                    break;
+                case "Contains":
+                    format = "({0} LIKE {1}+'%')";
+                    break;
+                default:
+                    throw new NotSupportedException(m.NodeType + " is not supported!");
+            }
 
+            this.Visit(m.Object);
+            this.Visit(m.Arguments[0]);
+            string right = this._StringStack.Pop();
+            string left = this._StringStack.Pop();
+            this._StringStack.Push(string.Format(format,left,right));
+
+            return m;
+        }
     }
 }
