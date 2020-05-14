f418cdb2db0e6fce168cbfe343bfef1f5aa35d0e
Adrian Sun <adrian_sun@livingstone.com.au>
2020-05-14 12:11:37 +1000

    ExpressionExtension, iqueryable extension.

---
+++ P02.Course.ExpressionSty/DBExtend/ExpressionExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using P05.Course.ExpressionSty.Visitor;
+
+namespace P05.Course.ExpressionSty.DBExtend
+{
+    public static class ExpressionExtension
+    {
+        public static void BatchDelete<T>(this IQueryable<T> entities, Expression<Func<T, bool>> expr)
+        {
+            ConditionBuilderVisitor visitor = new ConditionBuilderVisitor();
+            visitor.Visit(expr);
+            string condition = visitor.Condition();
+            // get by generic type T, condition may use T as well
+            string sql = string.Format("DELETE FROM [{0}] WHERE [{1}];",typeof(T).Name,condition);
+            // then execute the sql 
+        }
+
+
+
+
+
+    }
+}
