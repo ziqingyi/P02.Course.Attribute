81706ce1bea3e2782df13490a57db67b44625690
Adrian Sun <adrian_sun@livingstone.com.au>
2020-05-26 17:46:11 +1000

    directoryInfo not report error when creating new objects

--- P06.Course.IOSerialize/IO/MyIO.cs
+++ P06.Course.IOSerialize/IO/MyIO.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,19 @@ namespace P06.Course.IOSerialize.IO
 {
     public class MyIO
     {
+        private static string NotExistPath = ConfigurationManager.AppSettings["NotExistFolder"];
         private static string LogPath = ConfigurationManager.AppSettings["LogPath"];
         private static string LogMovePath = ConfigurationManager.AppSettings["LogMovePath"];
         private static string LogPath2 = AppDomain.CurrentDomain.BaseDirectory;
         public static void Show()
         {
+            if (Directory.Exists(LogPath))
+            {
+
+            }
+            DirectoryInfo notReportError = new DirectoryInfo(NotExistPath);
+
+
 
 
 
