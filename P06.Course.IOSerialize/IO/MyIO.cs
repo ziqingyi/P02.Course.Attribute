4da3108ebf06ede1fcc3d5bb9e022a21f375262b
Adrian Sun <adrian_sun@livingstone.com.au>
2020-05-26 17:27:31 +1000

    my IO show

---
+++ P06.Course.IOSerialize/IO/MyIO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06.Course.IOSerialize.IO
+{
+    public class MyIO
+    {
+        private static string LogPath = ConfigurationManager.AppSettings["LogPath"];
+        private static string LogMovePath = ConfigurationManager.AppSettings["LogMovePath"];
+        private static string LogPath2 = AppDomain.CurrentDomain.BaseDirectory;
+        public static void Show()
+        {
+
+
+
+        }
+
+
+
+    }
+}
