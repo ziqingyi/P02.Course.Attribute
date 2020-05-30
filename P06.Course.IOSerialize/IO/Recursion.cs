2fdf1acdb5d5ff5a1bcc0b219faa60a7c0e18a73
adrian sun <ziqingyi@qq.com>
2020-05-30 15:27:48 +1000

    recursion

---
+++ P06.Course.IOSerialize/IO/Recursion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06.Course.IOSerialize.IO
+{
+    public class Recursion
+    {
+        public static List<DirectoryInfo> GetAllDirectory(string rootPath)
+        {
+            if (!Directory.Exists(rootPath))
+            {
+                return new List<DirectoryInfo>();
+            }
+            List<DirectoryInfo> directoryList= new List<DirectoryInfo>();//container
+            DirectoryInfo directory = new DirectoryInfo(rootPath);//root folder
+            directoryList.Add(directory);
+
+            return GetChild(directoryList,directory);
+        }
+
+        private static List<DirectoryInfo> GetChild(List<DirectoryInfo> directoryList, DirectoryInfo directoryCurrent )
+        {
+            var childArray = directoryCurrent.GetDirectories();
+            if (childArray != null && childArray.Length > 0)
+            {
+                directoryList.AddRange(childArray);
+                foreach (var child in childArray)
+                {
+                    GetChild(directoryList, child);
+                }
+            }
+            return directoryList;
+        }
+    }
+}
--- P06.Course.IOSerialize/Program.cs
+++ P06.Course.IOSerialize/Program.cs
@@ -11,11 +11,17 @@ namespace P06.Course.IOSerialize
     {
         static void Main(string[] args)
         {
-            //MyIO.ShowDirectory();
-            //MyIO.ShowFile();
-            //MyIO.showDrive();
-            //MyIO.showPath();
-            MyIO.Log("log ....s.s.s.s..s.s.s");
+            {
+                //Console.WriteLine("******************IO Show, file, directory, recursion*************");
+                //MyIO.ShowDirectory();
+                //MyIO.ShowFile();
+                //MyIO.showDrive();
+                //MyIO.showPath();
+                //MyIO.Log("log ....s.s.s.s..s.s.s");
+                //var directoryInfo = Recursion.GetAllDirectory(@"C:\IOSerialize");
+            }
+
+
         }
     }
 }
