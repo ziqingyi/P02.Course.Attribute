7f03d9aa346865cef67faf80bb9f73535d4927a3
Adrian Sun <adrian_sun@livingstone.com.au>
2020-06-02 15:14:42 +1000

    linq to xml

---
+++ P06.Course.IOSerialize/Serialize/LinqToXml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06.Course.IOSerialize.Serialize
+{
+    public class LinqToXml
+    {
+
+
+
+
+
+    }
+}
