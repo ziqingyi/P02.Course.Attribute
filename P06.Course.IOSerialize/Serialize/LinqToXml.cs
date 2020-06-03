3cc43ff4b3a27dfdeb36bb24a64bc46844a67a3d
Adrian Sun <adrian_sun@livingstone.com.au>
2020-06-03 17:41:59 +1000

    GetXmlNodeInformation

--- P06.Course.IOSerialize/Serialize/LinqToXml.cs
+++ P06.Course.IOSerialize/Serialize/LinqToXml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -39,10 +40,50 @@ namespace P06.Course.IOSerialize.Serialize
                 Console.WriteLine(ex.ToString());
                 //throw;
             }
+        }
 
+        public static void GetXmlNodeInformation(string xmlPath)
+        {
+            try
+            {
+                //load root node
+                XElement rootNode = XElement.Load(xmlPath);
+                XElement rootNode2 = XElement.Parse(xmlPath);
+
+
+                //search the child nodes
+                IEnumerable<XElement> targetNodes = from target in rootNode.Descendants("name")
+                    select target;
+                foreach (XElement node in targetNodes)
+                {
+                    Console.WriteLine("name ={0}", node.Value);
+                }
+
+                //search user by id
+                IEnumerable<XElement> myTargetNodes = from myTarget in rootNode.Descendants("User")
+                    where myTarget.Attribute("ID").Value.Equals("1")
+                          && myTarget.HasElements
+                    select myTarget;
+                foreach (XElement node in myTargetNodes)
+                {
+                    Console.WriteLine("name = {0} ", node.Element("Name").Value);
+                    Console.WriteLine("password = {0} ", node.Element("Password").Value);
+                    Console.WriteLine("password = {0}", node.Element("Description").Value);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
 
 
         }
+
+
+
+
 
 
 
