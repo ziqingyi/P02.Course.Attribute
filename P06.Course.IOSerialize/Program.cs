3097086dfbf377ef8777f1efcdc949548de5d358
Adrian Sun <adrian_sun@livingstone.com.au>
2020-06-03 17:42:30 +1000

    test

--- P06.Course.IOSerialize/Program.cs
+++ P06.Course.IOSerialize/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,7 +48,7 @@ namespace P06.Course.IOSerialize
             {
                 Console.WriteLine("******************Xml Helper*******************************");
                 LinqToXml.CreateXmlFile(Constant.SerializeDataPath+@"\users.xml");
-
+                LinqToXml.GetXmlNodeInformation(Constant.SerializeDataPath + @"\users.xml");
             }
 
 
