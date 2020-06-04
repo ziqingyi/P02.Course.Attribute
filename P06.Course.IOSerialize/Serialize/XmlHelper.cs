fd541a6069180583fd80cf0264ca1f16c37bced9
Adrian Sun <adrian_sun@livingstone.com.au>
2020-06-04 17:35:59 +1000

    xmlserializer class type to string

--- P06.Course.IOSerialize/Serialize/XmlHelper.cs
+++ P06.Course.IOSerialize/Serialize/XmlHelper.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace P06.Course.IOSerialize.Serialize
 {
     public class XmlHelper
     {
-
+        //xmlserializer: from class type to string
+        public static string ToXml<T>(T t) where T : new()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(t.GetType());
+            Stream stream = new MemoryStream();
+            xmlSerializer.Serialize(stream, t);
+            stream.Position = 0;
+            StreamReader reader = new StreamReader(stream);
+            string text = reader.ReadToEnd();
+            return text;
+        }
 
 
 
