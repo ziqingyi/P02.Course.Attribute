470186df914e7539da5c64091e75932346f36e1a
adrian sun <ziqingyi@qq.com>
2020-06-14 22:06:46 +1000

    singleton pattern

---
+++ P08.Course.DesignPattern.Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08.Course.DesignPattern.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
