f48dbab53a40d7931d6923bdd87bca1a41632834
adrian sun <ziqingyi@qq.com>
2020-06-14 13:21:25 +1000

    DES decrypt and encrypt

---
+++ P07.Course.Encrypt/DesEncrypt.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07.Course.Encrypt
+{
+    public class DesEncrypt
+    {
+        private static byte[] _rgbKey = 
+            ASCIIEncoding.ASCII.GetBytes(
+                Constant.DesKey.Substring(0, 8));
+        private static byte[] _rgbIV = 
+            ASCIIEncoding.ASCII.GetBytes(
+                Constant.DesKey.Insert(0,"w").Substring(0,8));
+
+        //DES encrypt
+        public static string Encrypt(string text)
+        {
+            DESCryptoServiceProvider dsp = new DESCryptoServiceProvider();
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                CryptoStream crypStream = 
+                    new CryptoStream(memStream, 
+                        dsp.CreateEncryptor(_rgbKey,_rgbIV),
+                        CryptoStreamMode.Write
+                        );
+                StreamWriter sWriter = new StreamWriter(crypStream);
+                sWriter.Write(text);
+                sWriter.Flush();
+                crypStream.FlushFinalBlock();
+                memStream.Flush();
+                string result= Convert.ToBase64String(memStream.GetBuffer(),0,(int)memStream.Length);
+                return result;
+            }
+        }
+
+        //DES decrypt
+        public static string Decrypt(string encryptText)
+        {
+            DESCryptoServiceProvider dsp = new DESCryptoServiceProvider();
+            byte[] buffer = Convert.FromBase64String(encryptText);
+
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                CryptoStream crypStream = new CryptoStream(memStream,
+                    dsp.CreateDecryptor(_rgbKey, _rgbIV),
+                    CryptoStreamMode.Write);
+
+                crypStream.Write(buffer,0,buffer.Length);
+
+                crypStream.FlushFinalBlock();
+
+                string result = ASCIIEncoding.UTF8.GetString(memStream.ToArray());
+
+                return result;
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+}
--- P07.Course.Encrypt/Program.cs
+++ P07.Course.Encrypt/Program.cs
@@ -12,27 +12,36 @@ namespace P07.Course.Encrypt
         {
             #region MD5
             {
-                //same content with same MD5 value
-                Console.WriteLine(MD5Encrypt.Encrypt("1"));
-                Console.WriteLine(MD5Encrypt.Encrypt("1"));// same to the first one
+                ////same content with same MD5 value
+                //Console.WriteLine(MD5Encrypt.Encrypt("1"));
+                //Console.WriteLine(MD5Encrypt.Encrypt("1"));// same to the first one
 
-                Console.WriteLine(MD5Encrypt.Encrypt("123456Tom"));
+                //Console.WriteLine(MD5Encrypt.Encrypt("123456Tom"));
 
-                //compare same file with different name
-                string filepathWrong = @"‪C:\IOSerialize\user.xml";//wrong format, not support,C is different
-                string filepathWrongFixed = @"C:\IOSerialize\user.xml";// fixed with different C. 
-                byte[] wrong = Encoding.UTF8.GetBytes(filepathWrong);
-                byte[] correct = Encoding.UTF8.GetBytes(filepathWrongFixed); 
+                ////compare same file with different name
+                //string filepathWrong = @"‪C:\IOSerialize\user.xml";//wrong format, not support,C is different
+                //string filepathWrongFixed = @"C:\IOSerialize\user.xml";// fixed with different C. 
+                //byte[] wrong = Encoding.UTF8.GetBytes(filepathWrong);
+                //byte[] correct = Encoding.UTF8.GetBytes(filepathWrongFixed); 
                 
-                string filepath = @"C:\IOSerialize\user.xml";//correct format
-                string filepath2 = @"C:\IOSerialize\user2.xml";
-                string md5FileAbstract1 = MD5Encrypt.AbstractFile(filepath);
-                string md5FileAbstract2 = MD5Encrypt.AbstractFile(filepath2);
+                //string filepath = @"C:\IOSerialize\user.xml";//correct format
+                //string filepath2 = @"C:\IOSerialize\user2.xml";
+                //string md5FileAbstract1 = MD5Encrypt.AbstractFile(filepath);
+                //string md5FileAbstract2 = MD5Encrypt.AbstractFile(filepath2);
 
             }
             #endregion
 
+            #region DES, symmetric, the key of encrypt and decrypt is same. faster than asymmetric way. 
 
+            {
+                string desEn = DesEncrypt.Encrypt("1");
+                string desDe = DesEncrypt.Decrypt(desEn);
+
+                string desEn2 = DesEncrypt.Encrypt("2");
+                string desDe2 = DesEncrypt.Decrypt(desEn2);
+            }
+            #endregion
 
 
         }
