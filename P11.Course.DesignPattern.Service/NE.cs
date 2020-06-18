5d0fae7dba1fcc7374fc912a80f0e98da2bba0a2
adrian sun <ziqingyi@qq.com>
2020-06-18 22:06:26 +1000

    Simple factory structure

---
+++ P11.Course.DesignPattern.Service/NE.cs
@@ -0,0 +1,17 @@
+﻿using P10.Course.DesignPattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11.Course.DesignPattern.Service
+{
+    public class NE:IRace
+    {
+        public void ShowKing()
+        {
+            Console.WriteLine("The King of {0} is {1} ", this.GetType().Name, "Moon");
+        }
+    }
+}
