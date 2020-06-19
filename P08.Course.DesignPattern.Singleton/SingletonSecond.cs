98a541ca8bfe44f58a02be4f87ff2db2deb6d69c
adrian <adrian@DESKTOP-8Q9HP7L>
2020-06-20 00:01:34 +1000

    create instance by static constructor, and create prototype.

---
+++ P08.Course.DesignPattern.Singleton/SingletonSecond.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P08.Course.DesignPattern.Singleton
+{
+    public class SingletonSecond
+    {
+        private static SingletonSecond _singletonSecond = null;
+
+        static SingletonSecond()
+        {
+            _singletonSecond = new SingletonSecond();
+            Console.WriteLine("static constructor create instance...");
+        }
+
+        private SingletonSecond()
+        {
+            long _lResult = 0;
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                _lResult++;
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine("{0} is being created ", this.GetType().Name);
+        }
+
+        public SingletonSecond CreateInstancePrototype()
+        {
+            SingletonSecond second = (SingletonSecond)_singletonSecond.MemberwiseClone();
+            return second;
+        }
+
+
+
+
+    }
+}
