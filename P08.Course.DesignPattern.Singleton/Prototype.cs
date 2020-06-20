164cdbe434152602d8c9dd9ff3e9ad8345a3fc73
adrian sun <ziqingyi@qq.com>
2020-06-21 00:02:28 +1000

    create prototype

--- P08.Course.DesignPattern.Singleton/Prototype.cs
+++ P08.Course.DesignPattern.Singleton/Prototype.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace P08.Course.DesignPattern.Singleton
@@ -9,6 +10,30 @@ namespace P08.Course.DesignPattern.Singleton
     public class Prototype
     {
         //copy object from memory and then return new object, but not created from new()
+
+        //private constructor, consume resource when creating
+        private Prototype()
+        {
+            long iResult = 0;
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                iResult += 1;
+            }
+            Thread.Sleep(2000);
+            Console.WriteLine("private constructor is being called for {0}, iResult is: {1}",
+                this.GetType().Name, iResult);
+        }
+
+        // unique static instance
+        private static volatile Prototype _prototype = new Prototype();
+
+        //public method to provide new obj based on one obj
+        public static Prototype CreateInstance()
+        {
+            Prototype prototype = (Prototype)_prototype.MemberwiseClone();
+            return prototype;
+        }
+
 
 
 
