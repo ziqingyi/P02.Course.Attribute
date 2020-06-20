b7f23480abd42ea3dff6d2dcd9d884bf5b51e9c7
adrian sun <ziqingyi@qq.com>
2020-06-20 23:58:58 +1000

    prototype

---
+++ P08.Course.DesignPattern.Singleton/Prototype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08.Course.DesignPattern.Singleton
+{
+    public class Prototype
+    {
+        //copy object from memory and then return new object, but not created from new()
+
+
+
+
+    }
+}
