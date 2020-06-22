b911354d678177a3b2e5a33f78a3f25ef67e15d3
adrian sun <ziqingyi@qq.com>
2020-06-22 21:35:46 +1000

    human factory

---
+++ P13.Course.DesignPattern.AbstractFactory/Factory/HumanFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P10.Course.DesignPattern.Interface;
+using P11.Course.DesignPattern.Service;
+
+namespace P13.Course.DesignPattern.AbstractFactory.Factory
+{
+    public class HumanFactory : FactoryAbstract
+    {
+        public override IRace CreateRace()
+        {
+            return new Human();
+        }
+
+        public override IArmy CreateArmy()
+        {
+            return new HumanArmy();
+        }
+
+        public override IHero CreateHero()
+        {
+            return new HumanHero();
+        }
+
+        public override IResource CreateResource()
+        {
+            return new HumanResource();
+        }
+
+    }
+}
