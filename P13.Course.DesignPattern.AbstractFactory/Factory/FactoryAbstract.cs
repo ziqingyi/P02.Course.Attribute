f1b39be1dc61df072e202a74719b7fbef256c830
adrian sun <ziqingyi@qq.com>
2020-06-22 12:21:23 +1000

    abstract factory

---
+++ P13.Course.DesignPattern.AbstractFactory/Factory/FactoryAbstract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P10.Course.DesignPattern.Interface;
+
+namespace P13.Course.DesignPattern.AbstractFactory.Factory
+{
+    public abstract class FactoryAbstract
+    {
+        public abstract IRace CreateRace();
+        public abstract IArmy CreateArmy();
+        public abstract IHero CreateHero();
+        public abstract IResource CreateResource();
+        //public abstract ILuck CreateLuck();
+
+    }
+}
