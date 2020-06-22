434c67a051445359d7d12ee30c2475562de78a5d
adrian sun <ziqingyi@qq.com>
2020-06-22 21:46:55 +1000

    ORC factory and Undead Factory

---
+++ P13.Course.DesignPattern.AbstractFactory/Factory/ORCFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P10.Course.DesignPattern.Interface;
+using P11.Course.DesignPattern.Service;
+
+namespace P13.Course.DesignPattern.AbstractFactory.Factory
+{
+    public class ORCFactory :FactoryAbstract
+    {
+        public override IRace CreateRace()
+        {
+            return new ORC();
+        }
+
+        public override IArmy CreateArmy()
+        {
+            return new ORCArmy();
+        }
+
+        public override IHero CreateHero()
+        {
+            return new ORCHero();
+        }
+
+        public override IResource CreateResource()
+        {
+            return new ORCResource();
+        }
+
+    }
+}
---
+++ P13.Course.DesignPattern.AbstractFactory/Factory/UndeadFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P10.Course.DesignPattern.Interface;
+using P11.Course.DesignPattern.Service;
+
+namespace P13.Course.DesignPattern.AbstractFactory.Factory
+{
+    public class UndeadFactory :FactoryAbstract
+    {
+        public override IRace CreateRace()
+        {
+            return new Undead();
+        }
+
+        public override IArmy CreateArmy()
+        {
+            return new UndeadArmy();
+        }
+
+        public override IHero CreateHero()
+        {
+            return new UndeadHero();
+        }
+
+        public override IResource CreateResource()
+        {
+            return new UndeadResource();
+        }
+    }
+}
