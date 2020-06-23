b1ea996043a5000546f3e0cc831c131dfb9c3e4e
adrian sun <ziqingyi@qq.com>
2020-06-23 22:31:02 +1000

    test abstract factory

--- P13.Course.DesignPattern.AbstractFactory/Program.cs
+++ P13.Course.DesignPattern.AbstractFactory/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using P10.Course.DesignPattern.Interface;
+using P11.Course.DesignPattern.Service;
+using P13.Course.DesignPattern.AbstractFactory.Factory;
 
 namespace P13.Course.DesignPattern.AbstractFactory
 {
@@ -10,6 +13,33 @@ namespace P13.Course.DesignPattern.AbstractFactory
     {
         static void Main(string[] args)
         {
+            
+            {
+                // create instance separately.
+                IRace race = new Undead();
+                IArmy army = new UndeadArmy();
+                IHero hero = new UndeadHero();
+                IResource resource = new UndeadResource();
+            }
+            { 
+                //System.Data.SqlClient.SqlClientFactory// create instance together.
+                FactoryAbstract undeadFactory = new UndeadFactory();
+                IRace race = undeadFactory.CreateRace();
+                IArmy army = undeadFactory.CreateArmy();
+                IHero hero = undeadFactory.CreateHero();
+                IResource resource = undeadFactory.CreateResource();
+
+            }
+            {
+                FactoryAbstract orcfactory = new ORCFactory();
+                IRace race = orcfactory.CreateRace();
+                IArmy army = orcfactory.CreateArmy();
+                IHero hero = orcfactory.CreateHero();
+                IResource resource = orcfactory.CreateResource();
+            }
+
+
+
         }
     }
 }
