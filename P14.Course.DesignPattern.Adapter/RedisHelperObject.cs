23f94c081bfbb25ab18a07578a0bd9d325b8d502
adrian sun <ziqingyi@qq.com>
2020-06-25 23:03:06 +1000

    RedisHelperObject

---
+++ P14.Course.DesignPattern.Adapter/RedisHelperObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P14.Course.DesignPattern.Adapter
+{
+    public class RedisHelperObject : IHelper
+    {
+        public RedisHelperObject()
+        {
+            Console.WriteLine($"Construct {this.GetType().Name}");
+        }
+        private RedisHelper _redisHelper = new RedisHelper();//method1: must have old logic as instance
+
+        //method2: may be initialized in this way,not guarantee.
+        //but can pass a derived class instance
+        public RedisHelperObject(RedisHelper redisHelper)
+        {
+            this._redisHelper = redisHelper;
+        }
+
+
+
+        public void Add<T>()
+        {
+            this._redisHelper.AddRedis<T>();
+        }
+
+        public void Delete<T>()
+        {
+            this._redisHelper.DeleteRedis<T>();
+        }
+
+        public void Query<T>()
+        {
+            this._redisHelper.QueryRedis<T>();
+        }
+
+        public void Update<T>()
+        {
+            this._redisHelper.UpdateRedis<T>();
+        }
+
+
+    }
+}
