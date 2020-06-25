bbe13c8d94f8bb60aa426fd885800b815d5fbed9
adrian sun <ziqingyi@qq.com>
2020-06-25 22:55:12 +1000

    add old logic of cache, mysql, oracle, redis and sqlserver helper.

---
+++ P14.Course.DesignPattern.Adapter/CacheHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P14.Course.DesignPattern.Adapter
+{
+    public class CacheHelper
+    {
+        public void AddCache<T>()
+        {
+            Console.WriteLine("This is {0} Add", this.GetType().Name);
+        }
+
+        public void DeleteCache<T>()
+        {
+            Console.WriteLine("This is {0} Delete", this.GetType().Name);
+        }
+
+        public void UpdateCache<T>()
+        {
+            Console.WriteLine("This is {0} Update", this.GetType().Name);
+        }
+
+        public void QueryCache<T>()
+        {
+            Console.WriteLine("This is {0} Query", this.GetType().Name);
+        }
+
+    }
+}
---
+++ P14.Course.DesignPattern.Adapter/RedisHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P14.Course.DesignPattern.Adapter
+{
+    public class RedisHelper
+    {
+        public RedisHelper()
+        {
+            Console.WriteLine($" RedisHelper Constructor");
+        }
+        public void AddRedis<T>()
+        {
+            Console.WriteLine("This is {0} Add", this.GetType().Name);
+        }
+        public void DeleteRedis<T>()
+        {
+            Console.WriteLine("This is {0} Delete", this.GetType().Name);
+        }
+        public void UpdateRedis<T>()
+        {
+            Console.WriteLine("This is {0} Update", this.GetType().Name);
+        }
+        public void QueryRedis<T>()
+        {
+            Console.WriteLine("This is {0} Query", this.GetType().Name);
+        }
+    }
+}
