26cb385fac72d3e1c1b844220c82f3e2ec6438be
adrian sun <ziqingyi@qq.com>
2020-06-25 22:58:38 +1000

    redis helper inherit old logic of RedisHelper

---
+++ P14.Course.DesignPattern.Adapter/RedisHelperInherit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P14.Course.DesignPattern.Adapter
+{
+    public class RedisHelperInherit: RedisHelper,IHelper
+    {
+        public RedisHelperInherit()
+        {
+            Console.WriteLine($"Construct {this.GetType().Name}");
+        }
+
+        public void Add<T>()
+        {
+            base.AddRedis<T>();
+        }
+
+        public void Delete<T>()
+        {
+            base.DeleteRedis<T>();
+        }
+
+        public void Update<T>()
+        {
+            base.UpdateRedis<T>();
+        }
+
+        public void Query<T>()
+        {
+            base.QueryRedis<T>();
+        }
+    }
+}
