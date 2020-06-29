d05c21bfb79b672478eb385d2fe450dfe379dbff
adrian sun <ziqingyi@qq.com>
2020-06-29 14:14:36 +1000

    template method.

---
+++ P16.Course.DesignPattern.TemplateMethod/AbstractClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P16.Course.DesignPattern.TemplateMethod
+{
+    public abstract class AbstractClient
+    {
+        public void Query(int id, string name, string password)
+        {
+            if (this.CheckUser(id, password))
+            {
+                double balance = this.QueryBalance(id);
+                double interest = this.CalculateInterest(balance);
+            }
+            else
+            {
+                Console.WriteLine("wrong log in");
+            }
+        }
+
+        public bool CheckUser(int id, string password)
+        {
+            return DateTime.Now < DateTime.Now.AddDays(1);
+        }
+
+        public double QueryBalance(int id)
+        {
+            return new Random().Next(10000,1000000);
+        }
+
+        // abstract method, don't have implementation, means every derived class must have 
+        public abstract double CalculateInterest(double balance);
+
+        //default implementation, so use virtual method, derived class can have different implementation. 
+        public virtual void Show(string name, double balance, double interest)
+        {
+            Console.WriteLine("Hi {0}, your balance is {1}, interest is {2} ", name,balance,interest);
+        }
+
+
+    }
+}
