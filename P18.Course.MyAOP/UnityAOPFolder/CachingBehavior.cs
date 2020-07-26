e2090c390aa74f84d55a66a2dae26755d422957c
adrian_sun <ziqingyi@qq.com>
2020-07-26 12:43:36 +1000

    Caching Behavior

---
+++ P18.Course.MyAOP/UnityAOPFolder/CachingBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.PolicyInjection.Pipeline;
+
+namespace P18.Course.MyAOP.UnityAOPFolder
+{
+    public class CachingBehavior : IInterceptionBehavior
+    {
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            Console.WriteLine("Caching Behavior...");
+            if (input.MethodBase.Name.Equals("GetUser"))
+            {
+                return input.CreateMethodReturn(new User() { Id = 8, Name="user8", Password = "fdasde56536"});
+            }
+
+            return getNext().Invoke(input, getNext);
+        }
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+
+    }
+}
