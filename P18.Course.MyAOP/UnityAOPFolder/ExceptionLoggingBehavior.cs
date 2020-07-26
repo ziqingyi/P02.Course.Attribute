7e2a33cba48cbd363e7d47dcff2eee25fe19934b
adrian_sun <ziqingyi@qq.com>
2020-07-26 12:59:02 +1000

    ExceptionLoggingBehavior, LogAfterBehavior

---
+++ P18.Course.MyAOP/UnityAOPFolder/ExceptionLoggingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.PolicyInjection.Pipeline;
+
+namespace P18.Course.MyAOP.UnityAOPFolder
+{
+    public class ExceptionLoggingBehavior: IInterceptionBehavior
+    {
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            Console.WriteLine("Exception Logging Behavior");
+            IMethodReturn methodReturn = getNext()(input, getNext);
+            if (methodReturn.Exception == null)
+            {
+                Console.WriteLine("No exception");
+            }
+            else
+            {
+                Console.WriteLine($" Exception: {methodReturn.Exception.Message}");
+            }
+
+            return methodReturn;
+        }
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+
+
+    }
+}
---
+++ P18.Course.MyAOP/UnityAOPFolder/LogAfterBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.PolicyInjection.Pipeline;
+
+namespace P18.Course.MyAOP.UnityAOPFolder
+{
+    public class LogAfterBehavior: IInterceptionBehavior
+    {
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            Console.WriteLine("Log After Behavior");
+            foreach (var item in input.Inputs)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            IMethodReturn methodReturn = getNext()(input, getNext);
+
+            Console.WriteLine("Log After Behavior "+ methodReturn.ReturnValue);
+
+            return methodReturn;
+        }
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+
+    }
+}
