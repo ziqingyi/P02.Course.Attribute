3e9fa55a91840188c759f44c855edb8ce0585928
adrian_sun <ziqingyi@qq.com>
2020-07-26 13:01:52 +1000

    UnityUserProcessor

---
+++ P18.Course.MyAOP/UnityAOPFolder/UnityUserProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P18.Course.MyAOP.UnityAOPFolder
+{
+    public class UnityUserProcessor
+    {
+        public void RegUser(User user)
+        {
+            Console.WriteLine("User {0} has registered successfully...", user.Name);
+        }
+
+        public User GetUser(User user)
+        {
+            return user;
+        }
+
+    }
+}
