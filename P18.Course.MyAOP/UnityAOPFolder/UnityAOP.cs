432a38c9f11c8bf03eeca55c12714b2b29ba764e
adrian_sun <ziqingyi@qq.com>
2020-07-26 13:17:26 +1000

    unityAOP

---
+++ P18.Course.MyAOP/UnityAOPFolder/UnityAOP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P18.Course.MyAOP.UnityAOPFolder
+{
+    public class UnityAOP
+    {
+        public static void Show()
+        {
+            User user = new User()
+            {
+                Id = 7,
+                Name = "user7",
+                Password = "afdr34354yt"
+            };
+
+
+
+
+
+        }
+
+
+    }
+}
