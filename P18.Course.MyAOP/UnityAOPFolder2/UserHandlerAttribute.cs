5bf9e02594270dc0cdf3e0496ae1487c38f4b74f
adrian_sun <ziqingyi@qq.com>
2020-07-28 14:41:56 +1000

    UserHandlerAttribute

---
+++ P18.Course.MyAOP/UnityAOPFolder2/UserHandlerAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Interception.PolicyInjection.Pipeline;
+using Unity.Interception.PolicyInjection.Policies;
+
+namespace P18.Course.MyAOP.UnityAOPFolder2
+{
+    public class UserHandlerAttribute : HandlerAttribute
+    {
+        public override ICallHandler CreateHandler(IUnityContainer container)
+        {
+            ICallHandler handler = new UserHandler()
+            {
+                Order = this.Order
+            };
+
+            return handler;
+        }
+
+
+    }
+}
