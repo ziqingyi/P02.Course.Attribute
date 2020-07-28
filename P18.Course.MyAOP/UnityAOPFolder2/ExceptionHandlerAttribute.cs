45cd2c882f00d6e18189d1980b6e347b615d5614
adrian_sun <ziqingyi@qq.com>
2020-07-28 14:46:32 +1000

    ExceptionHandlerAttribute

---
+++ P18.Course.MyAOP/UnityAOPFolder2/ExceptionHandlerAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Interception.PolicyInjection.Pipeline;
+using Unity.Interception.PolicyInjection.Policies;
+
+namespace P18.Course.MyAOP.UnityAOPFolder2
+{
+    public class ExceptionHandlerAttribute: HandlerAttribute
+    {
+        public override ICallHandler CreateHandler(IUnityContainer container)
+        {
+            return new ExceptionHandler()
+            {
+                Order = this.Order
+            };
+        }
+
+
+    }
+}
