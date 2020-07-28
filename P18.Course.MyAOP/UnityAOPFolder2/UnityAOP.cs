64d3c001604d860637d113638cfb8b17c9186407
adrian_sun <ziqingyi@qq.com>
2020-07-28 14:54:43 +1000

    show UnityAOP

--- P18.Course.MyAOP/UnityAOPFolder2/UnityAOP.cs
+++ P18.Course.MyAOP/UnityAOPFolder2/UnityAOP.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
+using Unity.Interception;
+using Unity.Interception.Interceptors.InstanceInterceptors.InterfaceInterception;
 
 namespace P18.Course.MyAOP.UnityAOPFolder2
 {
@@ -17,9 +20,20 @@ namespace P18.Course.MyAOP.UnityAOPFolder2
                 Password = "afdr34354yt"
             };
 
-            
+            IUnityContainer container = new UnityContainer();// a container
+            container.RegisterType<IUnityUserProcessor2, UnityUserProcessor2>();
+            IUnityUserProcessor2 processor2 = container.Resolve<IUnityUserProcessor2>();
+            processor2.RegUser(user);
 
+            container.AddNewExtension<Interception>();
+            container.RegisterType<IUnityUserProcessor2, UnityUserProcessor2>()
+                .Configure<Interception>()
+                .SetInterceptorFor<IUnityUserProcessor2>(new InterfaceInterceptor());
 
+            IUnityUserProcessor2 userProcessor2 = new UnityUserProcessor2();
+            IUnityUserProcessor2 userProcessor22 = container.Resolve<IUnityUserProcessor2>();
+            userProcessor2.RegUser(user);
+            userProcessor2.GetUser(user);
 
         }
 
