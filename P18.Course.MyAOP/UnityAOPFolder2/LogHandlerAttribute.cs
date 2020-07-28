0a60d0c5619ede1b0ce5741f239840ec3c6c2d24
adrian_sun <ziqingyi@qq.com>
2020-07-28 14:44:08 +1000

    LogHandlerAttribute

---
+++ P18.Course.MyAOP/UnityAOPFolder2/LogHandlerAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Interception.PolicyInjection.Pipeline;
+using Unity.Interception.PolicyInjection.Policies;
+
+namespace P18.Course.MyAOP.UnityAOPFolder2
+{
+    public class LogHandlerAttribute: HandlerAttribute
+    {
+        public override ICallHandler CreateHandler(IUnityContainer container)
+        {
+            LogHandler result = new LogHandler()
+            {
+                Order = this.Order
+            };
+            return result;
+        }
+
+
+    }
+}
