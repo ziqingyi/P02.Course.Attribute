5f2dfc5e53d6c9a6067e5c3370356fcc24f388e4
adrian_sun <ziqingyi@qq.com>
2020-07-29 09:20:53 +1000

    comment for virtual

--- P18.Course.MyAOP/CastleProxyAOPFolder/CastleProxyUserProcessor.cs
+++ P18.Course.MyAOP/CastleProxyAOPFolder/CastleProxyUserProcessor.cs
@@ -8,7 +8,10 @@ namespace P18.Course.MyAOP.CastleProxyAOPFolder
 {
     public class CastleProxyUserProcessor:IUserProcessor
     {
-        // must be virtual
+        // must be virtual, because the reference is from another class CastleProxyUserProcessor.
+        //       and virtual method selection is decided by right side. 
+        // if use normal method, the method being called will not be intercepted. because 
+        //      the method selection is decided by left side. 
         public virtual void RegUser(User user)
         { 
             Console.WriteLine("user {0}--{1} is being registered with password: {2}",
