66f4fc0e3feb28257770bea60259881f087a76ac
adrian_sun <ziqingyi@qq.com>
2020-07-31 21:07:16 +1000

    test exception

--- P18.Course.MyAOP/Program.cs
+++ P18.Course.MyAOP/Program.cs
@@ -17,39 +17,47 @@ namespace P18.Course.MyAOP
     {
         static void Main(string[] args)
         {
-            #region AOP show
-            //Implementing AOP:
-            //1 static way: The Decorator/Proxy Design Pattern
-            //2 dynamic way: Creating a Dynamic Proxy with RealProxy(remoting), Castle(Emit)
-            //3 Compile-Time/Runtime Weaving: PostSharp(), the
-            //     program is modified during the build process on the development machine, before deployment.
-            //     the program is modified during its execution, after deployment
-
-            //4 Independent injection with aop extension: Unity and AutoFac, commonly use other than MVC.
-
-            //5 MVC AOP: invoker center, use attribute and reflection to decide the execution of some other logic. 
-
-            //Console.WriteLine("****************Decorator Show********************");
-            //DecoratorAOP.Show();
-
-            //Console.WriteLine("************Proxy AOP Show*******************");
-            //ProxyAOP.Show();
-
-            //Console.WriteLine("************Real Proxy AOP Show*******************");
-            //RealProxyAOP.Show();
-
-            //Console.WriteLine("***************Castle Proxy AOP show**************");
-            //CastleProxyAOP.Show();
-
-            Console.WriteLine("***************Unity AOP show**************");
-            UnityConfigAOP.Show();
-
-            UnityAOP.Show();
-
-            #endregion
+            try
+            {
 
 
+                #region AOP show
 
+                //Implementing AOP:
+                //1 static way: The Decorator/Proxy Design Pattern
+                //2 dynamic way: Creating a Dynamic Proxy with RealProxy(remoting), Castle(Emit)
+                //3 Compile-Time/Runtime Weaving: PostSharp(), the
+                //     program is modified during the build process on the development machine, before deployment.
+                //     the program is modified during its execution, after deployment
+
+                //4 Independent injection with aop extension: Unity and AutoFac, commonly use other than MVC.
+
+                //5 MVC AOP: invoker center, use attribute and reflection to decide the execution of some other logic. 
+
+                //Console.WriteLine("****************Decorator Show********************");
+                //DecoratorAOP.Show();
+
+                //Console.WriteLine("************Proxy AOP Show*******************");
+                //ProxyAOP.Show();
+
+                //Console.WriteLine("************Real Proxy AOP Show*******************");
+                //RealProxyAOP.Show();
+
+                //Console.WriteLine("***************Castle Proxy AOP show**************");
+                //CastleProxyAOP.Show();
+
+                Console.WriteLine("***************Unity AOP show**************");
+                UnityConfigAOP.Show();
+
+                UnityAOP.Show();
+
+                #endregion
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
