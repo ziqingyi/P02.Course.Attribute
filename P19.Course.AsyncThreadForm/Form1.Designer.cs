f9efbb77f6008e76a93c88fc43298c6369e3a7b5
adrian_sun <ziqingyi@qq.com>
2020-08-03 09:15:37 +1000

    P19 btnSync

---
+++ P19.Course.AsyncThreadForm/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace P19.Course.AsyncThreadForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(52, 55);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(75, 23);
+            this.btnSync.TabIndex = 0;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(52, 117);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnAsync.TabIndex = 1;
+            this.btnAsync.Text = "Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnSync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnAsync;
+    }
+}
+
---
+++ P19.Course.AsyncThreadForm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P19.Course.AsyncThreadForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSync_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine($"*************button Sync Click Start" +
+                              $"{Thread.CurrentThread.ManagedThreadId.ToString("00")}" +
+                              $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss. fff")}" +
+                              $"*********************");
+
+            int l = 3;
+            int m = 4;
+            int n = l + m;
+            for (int i = 0; i < 5; i++)
+            {
+                string name = string.Format($"btnSync_Click_{i}");
+                this.DoSomethingLong(name);
+            }
+            Console.WriteLine($"****************btnSync_Click End " +
+                              $"{Thread.CurrentThread.ManagedThreadId.ToString("00")} " +
+                              $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}" +
+                              $"***************");
+
+        }
+
+
+        // a time consuming task
+        private void DoSomethingLong(string name)
+        {
+            Console.WriteLine($"****************DoSomethingLong Start  {name}  " +
+                              $"{Thread.CurrentThread.ManagedThreadId.ToString("00")} " +
+                              $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}" +
+                              $"***************");
+            long lResult = 0;
+            for (int i = 0; i < 1_000_000_000; i++)
+            {
+                lResult += i;
+            }
+            //Thread.Sleep(2000);
+
+            Console.WriteLine($"****************DoSomethingLong   End  " +
+                              $"{name}  " +
+                              $"{Thread.CurrentThread.ManagedThreadId.ToString("00")} " +
+                              $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} " +
+                              $"{lResult}***************");
+
+        }
+
+        private void btnAsync_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
