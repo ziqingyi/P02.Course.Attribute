31095b8b224c3a9272fc0b0d0f1e398de0258f1e
adrian_sun <ziqingyi@qq.com>
2020-08-26 09:13:16 +1000

    lottery project

---
+++ P21.Course.Lottery/frmLottery.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace P21.Course.Lottery
+{
+    partial class frmLottery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxDisplay = new System.Windows.Forms.GroupBox();
+            this.lblRed1 = new System.Windows.Forms.Label();
+            this.lblRed2 = new System.Windows.Forms.Label();
+            this.lblRed3 = new System.Windows.Forms.Label();
+            this.lblRed4 = new System.Windows.Forms.Label();
+            this.lblRed5 = new System.Windows.Forms.Label();
+            this.lblRed6 = new System.Windows.Forms.Label();
+            this.lblBlue1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBoxDisplay.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxDisplay
+            // 
+            this.groupBoxDisplay.Controls.Add(this.btnStop);
+            this.groupBoxDisplay.Controls.Add(this.btnStart);
+            this.groupBoxDisplay.Controls.Add(this.lblBlue1);
+            this.groupBoxDisplay.Controls.Add(this.lblRed6);
+            this.groupBoxDisplay.Controls.Add(this.lblRed5);
+            this.groupBoxDisplay.Controls.Add(this.lblRed4);
+            this.groupBoxDisplay.Controls.Add(this.lblRed3);
+            this.groupBoxDisplay.Controls.Add(this.lblRed2);
+            this.groupBoxDisplay.Controls.Add(this.lblRed1);
+            this.groupBoxDisplay.Location = new System.Drawing.Point(48, 74);
+            this.groupBoxDisplay.Name = "groupBoxDisplay";
+            this.groupBoxDisplay.Size = new System.Drawing.Size(498, 189);
+            this.groupBoxDisplay.TabIndex = 0;
+            this.groupBoxDisplay.TabStop = false;
+            this.groupBoxDisplay.Text = "DisplayArea";
+            // 
+            // lblRed1
+            // 
+            this.lblRed1.AutoSize = true;
+            this.lblRed1.ForeColor = System.Drawing.Color.Red;
+            this.lblRed1.Location = new System.Drawing.Point(44, 66);
+            this.lblRed1.Name = "lblRed1";
+            this.lblRed1.Size = new System.Drawing.Size(19, 13);
+            this.lblRed1.TabIndex = 0;
+            this.lblRed1.Text = "00";
+            // 
+            // lblRed2
+            // 
+            this.lblRed2.AutoSize = true;
+            this.lblRed2.ForeColor = System.Drawing.Color.Red;
+            this.lblRed2.Location = new System.Drawing.Point(89, 66);
+            this.lblRed2.Name = "lblRed2";
+            this.lblRed2.Size = new System.Drawing.Size(19, 13);
+            this.lblRed2.TabIndex = 1;
+            this.lblRed2.Text = "00";
+            // 
+            // lblRed3
+            // 
+            this.lblRed3.AutoSize = true;
+            this.lblRed3.ForeColor = System.Drawing.Color.Red;
+            this.lblRed3.Location = new System.Drawing.Point(134, 66);
+            this.lblRed3.Name = "lblRed3";
+            this.lblRed3.Size = new System.Drawing.Size(19, 13);
+            this.lblRed3.TabIndex = 2;
+            this.lblRed3.Text = "00";
+            // 
+            // lblRed4
+            // 
+            this.lblRed4.AutoSize = true;
+            this.lblRed4.ForeColor = System.Drawing.Color.Red;
+            this.lblRed4.Location = new System.Drawing.Point(200, 66);
+            this.lblRed4.Name = "lblRed4";
+            this.lblRed4.Size = new System.Drawing.Size(19, 13);
+            this.lblRed4.TabIndex = 3;
+            this.lblRed4.Text = "00";
+            // 
+            // lblRed5
+            // 
+            this.lblRed5.AutoSize = true;
+            this.lblRed5.ForeColor = System.Drawing.Color.Red;
+            this.lblRed5.Location = new System.Drawing.Point(240, 66);
+            this.lblRed5.Name = "lblRed5";
+            this.lblRed5.Size = new System.Drawing.Size(19, 13);
+            this.lblRed5.TabIndex = 4;
+            this.lblRed5.Text = "00";
+            // 
+            // lblRed6
+            // 
+            this.lblRed6.AutoSize = true;
+            this.lblRed6.ForeColor = System.Drawing.Color.Red;
+            this.lblRed6.Location = new System.Drawing.Point(291, 66);
+            this.lblRed6.Name = "lblRed6";
+            this.lblRed6.Size = new System.Drawing.Size(19, 13);
+            this.lblRed6.TabIndex = 5;
+            this.lblRed6.Text = "00";
+            // 
+            // lblBlue1
+            // 
+            this.lblBlue1.AutoSize = true;
+            this.lblBlue1.ForeColor = System.Drawing.Color.Blue;
+            this.lblBlue1.Location = new System.Drawing.Point(410, 66);
+            this.lblBlue1.Name = "lblBlue1";
+            this.lblBlue1.Size = new System.Drawing.Size(19, 13);
+            this.lblBlue1.TabIndex = 6;
+            this.lblBlue1.Text = "00";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(73, 118);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 33);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(311, 118);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(77, 31);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // frmLottery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 314);
+            this.Controls.Add(this.groupBoxDisplay);
+            this.Name = "frmLottery";
+            this.Text = "frmLottery";
+            this.groupBoxDisplay.ResumeLayout(false);
+            this.groupBoxDisplay.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxDisplay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblBlue1;
+        private System.Windows.Forms.Label lblRed6;
+        private System.Windows.Forms.Label lblRed5;
+        private System.Windows.Forms.Label lblRed4;
+        private System.Windows.Forms.Label lblRed3;
+        private System.Windows.Forms.Label lblRed2;
+        private System.Windows.Forms.Label lblRed1;
+    }
+}
+
---
+++ P21.Course.Lottery/frmLottery.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P21.Course.Lottery
+{
+    public partial class frmLottery : Form
+    {
+        public frmLottery()
+        {
+            InitializeComponent();
+            this.btnStart.Enabled = true;
+            this.btnStop.Enabled = false;
+        }
+
+        private string[] RedNums =
+        {
+            "01","02","03","04","05","06","07","08","09","10",
+            "11","12","13","14","15","16","17","18","19","20",
+            "21","22","23","24","25","26","27","28","29","30",
+            "31","32","33"
+        };
+
+        private string[] BlueNums =
+        {
+            "01","02","03","04","05","06","07","08","09","10",
+            "11","12","13","14","15","16"
+        };
+
+        private bool IsGoOn = true;
+        private List<Task> taskList = new List<Task>();
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
