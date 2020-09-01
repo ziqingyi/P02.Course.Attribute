72b04a918b65ec912fc4b6c5b105b040c7fd5b6c
adrian_sun <ziqingyi@qq.com>
2020-09-02 09:09:42 +1000

    models relating to search

---
+++ P22.Course.Crawler/CommoditySearch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P22.Course.Crawler
+{
+    public class CommoditySearch: ISearch
+    {
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ P22.Course.Crawler/Model/CommodityPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P22.Course.Crawler.Model
+{
+    class CommodityPrice
+    {
+        public string id { get; set; }
+        public decimal p { get; set; }
+        public decimal m { get; set; }
+
+    }
+}
