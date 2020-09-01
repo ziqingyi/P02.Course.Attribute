c0848eae425879ae11582740f8cd79a23a2b99e7
adrian_sun <ziqingyi@qq.com>
2020-09-02 08:59:55 +1000

    ISearch interface

---
+++ P22.Course.Crawler/ISearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P22.Course.Crawler
+{
+    interface ISearch
+    {
+        void Crawler();
+    }
+}
