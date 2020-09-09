42279cc775f484a9caeb57e44f3ab7605b289d53
adrian_sun <ziqingyi@qq.com>
2020-09-09 17:43:14 +1000

    base DAL lib

---
+++ P22.Course.IOC.DAL/BaseDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P22.Course.IOC.DAL
+{
+    public class BaseDAL
+    {
+        public BaseDAL()
+        {
+            Console.WriteLine($"{nameof(BaseDAL)} is being constructed. ");
+        }
+
+        public void Add()
+        {
+            Console.WriteLine($"public method {nameof(Add)} ");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine($"public method {nameof(Delete)} ");
+        }
+
+        public void Find()
+        {
+            Console.WriteLine($"public method {nameof(Find)} ");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine($"public method {nameof(Update)} ");
+        }
+    }
+}
