1683d3faa25caa79dcf6b9b37d4a5826edac8d61
adrian_sun <ziqingyi@qq.com>
2020-09-09 17:13:46 +1000

    IOC and packages

---
+++ P23.Course.IOC.Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P23.Course.IOC.Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
