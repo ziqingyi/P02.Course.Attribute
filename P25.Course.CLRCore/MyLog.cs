fd902de9e0039bcfb32c70515c0b921b71430cc5
adrian_sun <ziqingyi@qq.com>
2020-10-20 09:30:01 +1100

    mylog

---
+++ P25.Course.CLRCore/MyLog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P25.Course.CLRCore
+{
+    public class MyLog
+    {
+        public static void Log(string msg)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                string fileName = "log.txt";
+                string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+                sw = File.AppendText(fullPath);
+
+                sw.WriteLine(string.Format(" {0} : {1} ",DateTime.Now,msg));
+                sw.WriteLine("***************************************************");
+
+                Console.WriteLine(msg);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.Flush();
+                    sw.Close();
+                    sw.Dispose();
+                }
+
+            }
+
+
+
+        }
+
+
+    }
+}
