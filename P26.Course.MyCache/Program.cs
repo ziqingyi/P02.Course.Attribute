ca693e16fbf93ee5463af87a9f5de3b2cba4c6d0
adrian_sun <ziqingyi@qq.com>
2020-10-24 17:36:56 +1100

    CustomCache with checking logic using func

--- P26.Course.MyCache/Program.cs
+++ P26.Course.MyCache/Program.cs
@@ -22,14 +22,14 @@ namespace P26.Course.MyCache
                 int param = 123;
                 string methodName =typeof(DBHelper)+"."+nameof(DBHelper.Query);
 
-                //normal way without cache
+                //method 1 : normal way without cache
                 for (int i = 0; i < 5; i++)
                 {
                     Console.WriteLine($"try to get {methodName} in seq: {i} at {DateTime.Now.ToString("yyyyMMdd HHmmss.fff")}");
                     List<TestClass> programList = DBHelper.Query<TestClass>(param);
                 }
 
-                //cache to class CustomCache's static field, a dictionary. 
+                //method 2 : cache to class CustomCache's static field, a dictionary. 
                 for (int i = 0; i < 5; i++)
                 {
                     Console.WriteLine($"try to get {methodName} in seq: {i} at {DateTime.Now.ToString("yyyyMMdd HHmmss.fff")}");
@@ -50,12 +50,29 @@ namespace P26.Course.MyCache
                     }
                 }
 
+                //method 3: using cache in DBHelper, encapsulate check existing and generating instance logic inside. 
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(
+                        $"try to get {methodName} in seq: {i} at {DateTime.Now.ToString("yyyyMMdd HHmmss.fff")}");
+                    List<TestClass> programList = null;
+
+                    //build key based on T and param, so same T and param will be same result and can get from cache in the following.
+                    string key = $"{methodName}_Parameters_{param}";
+
+                    programList = CustomCache.GetT<List<TestClass>>(key, () => DBHelper.Query<TestClass>(param));
+
+
+                }
+
+
+
                 #endregion
 
 
 
 
-            }
+                }
 
 
 
