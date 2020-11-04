5e1373a7c2c2377408d7ff3beed291e5228be42d
adrian_sun <ziqingyi@qq.com>
2020-11-04 14:00:22 +1100

    dynamic check

--- P27.Course.DataStructure/DynamicDemo.cs
+++ P27.Course.DataStructure/DynamicDemo.cs
@@ -52,13 +52,15 @@ namespace P27.Course.DataStructure
                 MethodInfo method = type.GetMethod("Power");
                 object result = method.Invoke(a, null);
 
-
+            
                 //use dynamic way other than reflection, 1 efficient data-binding 2 interoperable with C++
                 dynamic dynamic_a = a;
                 dynamic_a.Power();
 
+
+                //clr will generate several delegates in first time running, then next time, will invoke directly, faster than reflection. 
                 dynamic str = "abcd";
-                Console.WriteLine(str.Length);
+                Console.WriteLine(str.Length);// everything in str will be dynamic. so Length, Substring() are also dynamic. 
                 Console.WriteLine(str.Substring(1));
             }
 
---
+++ P27.Course.DataStructure/DynamicDemoILSpy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P27.Course.DataStructure
+{
+    class DynamicDemoILSpy
+    {
+        //check in IL Spy
+        public void test()
+        {
+            //clr will generate several delegates in first time running, then next time, will invoke directly, faster than reflection. 
+            dynamic str = "abcd";
+            Console.WriteLine(str.Length);// everything in str will be dynamic. so Length, Substring() are also dynamic. 
+            Console.WriteLine(str.Substring(1));
+        }
+
+    }
+}
