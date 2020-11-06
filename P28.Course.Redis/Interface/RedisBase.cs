fc575b6f16b3a433103c6f9b9ba5b27d858376a5
adrian_sun <ziqingyi@qq.com>
2020-11-06 17:41:58 +1100

    RedisBase

--- P28.Course.Redis/Interface/RedisBase.cs
+++ P28.Course.Redis/Interface/RedisBase.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using P28.Course.Redis.Init;
 
 namespace P28.Course.Redis.Interface
 {
@@ -13,12 +14,64 @@ namespace P28.Course.Redis.Interface
 
         public RedisBase()
         {
-            iClient = RedisManager
+            iClient = RedisManager.GetClient();
         }
 
+        private bool _disposed = false;
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    iClient.Dispose();
+                    iClient = null;
+                }
+            }
+            this._disposed = true;
+        }
 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
+        public void Transaction()
+        {
+            using (IRedisTransaction irt = this.iClient.CreateTransaction())
+            {
+                try
+                {
+                    irt.QueueCommand(r =>r.Set("key",20));
+                    irt.QueueCommand(r=>r.Increment("key",1));
+                    irt.Commit();
+                }
+                catch (Exception ex)
+                {
+                    irt.Rollback();
+                    throw;
+                }
+            }
+        }
 
+        //clear all data, please be aware
+        public virtual void FlushAll()
+        {
+            iClient.FlushAll();
+        }
+
+        //save data to hard drive
+        public void Save()
+        {
+            iClient.Save();//blocking way of saving
+        }
+
+        // save data to hard drive asynchronous  
+        public void SaveAsync()
+        {
+            iClient.SaveAsync();
+        }
     }
 }
