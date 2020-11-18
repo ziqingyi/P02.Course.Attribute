63bf56f6d9e2665c136a834658d607fcfd8eeba5
adrian_sun <ziqingyi@qq.com>
2020-11-18 16:47:10 +1100

    calculator sesrvice and math service

---
+++ P30.Course.SOA.WCF.Service/CalculatorService.cs
@@ -0,0 +1,29 @@
+﻿using P30.Course.SOA.WCF.Interface;
+using P30.Course.SOA.WCF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P30.Course.SOA.WCF.Service
+{
+
+    public class CalculatorService : ICalculatorService
+    {
+
+        public void Plus(int x, int y)
+        {
+            int result = x + y;
+            Thread.Sleep(2000);
+
+            ICallBack callBack = OperationContext.Current.GetCallbackChannel<ICallBack>();
+            callBack.Show(x,y,result);
+
+        }
+
+
+    }
+}
---
+++ P30.Course.SOA.WCF.Service/MathService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P30.Course.SOA.WCF.Interface;
+using P30.Course.SOA.WCF.Model;
+
+namespace P30.Course.SOA.WCF.Service
+{
+    public class MathService:IMathService
+    {
+
+        private List<WCFUser> uList = new List<WCFUser>()
+        {
+            new WCFUser()
+            {
+                Id = 1,
+                Name = "user1",
+                Sex = (int)WCFUserSex.Male,
+                Age = 29,
+                Description = "123456678990"
+            },
+            new WCFUser()
+            {
+                Id = 2,
+                Name = "user2",
+                Sex = (int)WCFUserSex.Male,
+                Age = 22,
+                Description = "123456678990"
+            },
+            new WCFUser()
+            {
+                Id = 3,
+                Name = "user3",
+                Sex = (int)WCFUserSex.Female,
+                Age = 32,
+                Description = "123456678990"
+            }
+        };
+        public int PlusInt(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Minus(int x, int y)
+        {
+            return x - y;
+        }
+
+        public WCFUser GetUser(int x, int y)
+        {
+            int tid = x + y;
+            WCFUser wcfUser = new WCFUser()
+            {
+                Id = tid,
+                Name = "User_" + tid,
+                Age = 22,
+                Description = "WCF Service user " + tid,
+                Sex = (int)WCFUserSex.Female
+            };
+
+            return wcfUser;
+        }
+
+        public List<WCFUser> UserList()
+        {
+            return uList;
+        }
+
+
+
+
+
+    }
+}
