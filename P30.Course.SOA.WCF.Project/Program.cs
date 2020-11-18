8722c960dca99aeea91c6317c346b3c20cd23786
adrian_sun <ziqingyi@qq.com>
2020-11-18 17:44:52 +1100

    wcf project service init

---
+++ P30.Course.SOA.WCF.Project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P30.Course.SOA.WCF.Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("**********SOA  WCF***************");
+
+                ServiceInit.Process();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+        }
+    }
+}
---
+++ P30.Course.SOA.WCF.Project/ServiceInit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using P30.Course.SOA.WCF.Service;
+
+namespace P30.Course.SOA.WCF.Project
+{
+    public class ServiceInit
+    {
+        public static void Process()
+        {
+
+            //ServiceHost object
+            List<ServiceHost> hosts = new List<ServiceHost>()
+            {
+                new ServiceHost(typeof(MathService)),
+                new ServiceHost(typeof(CalculatorService))
+            };
+
+            foreach (ServiceHost host in hosts)
+            {
+                host.Opening += (s, e) => Console.WriteLine($"{host.GetType().Name} start to open....");
+                host.Opened += (s,e)=>Console.WriteLine($"{host.GetType().Name} is open");
+                host.Closing += (s, e) => Console.WriteLine($"{host.GetType().Name} is closing....");
+                host.Closed += (s, e) => Console.WriteLine($"{host.GetType().Name} is closed");
+
+                host.Open();
+            }
+
+
+
+            Console.WriteLine("stop when receiving any character......");
+            Console.Read();
+            foreach (ServiceHost host in hosts)
+            {
+                host.Close();
+            }
+
+            Console.Read();
+
+        }
+
+
+        private static void Host_Closing(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
