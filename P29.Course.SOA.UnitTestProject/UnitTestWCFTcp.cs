ff311bcc0cabd87ec1b69814ca8d1b8c1f8a83a4
adrian_sun <ziqingyi@qq.com>
2020-11-19 11:44:52 +1100

    test WCF TCP math service

---
+++ P29.Course.SOA.UnitTestProject/UnitTestWCFTcp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P29.Course.SOA.UnitTestProject.MyConsoleWCFTcpTest;
+
+namespace P29.Course.SOA.UnitTestProject
+{
+    [TestClass]
+    public class UnitTestWCFTcp
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MyConsoleWCFTcpTest.MathServiceClient client = null;
+            try
+            {
+                client = new MathServiceClient();
+
+                WCFUser wcfUser = client.GetUser(222, 666);
+
+
+
+            }
+            catch (Exception ex)
+            {
+                if (client != null)
+                {
+                    client.Abort();
+                }
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+
+
+
+    }
+}
