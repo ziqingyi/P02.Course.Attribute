d7a675dedfce4758bda63a95afcb8e063724104a
adrian_sun <ziqingyi@qq.com>
2020-11-22 15:21:38 +1100

    users controller, get

---
+++ P31.Course.SOA.Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P31.Course.SOA.Model
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string userEmail { get; set; }
+    }
+}
---
+++ P31.Course.SOA.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+using P31.Course.SOA.Interface;
+using P31.Course.SOA.Model;
+
+namespace P31.Course.SOA.WebAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private IUserService _userService = null;
+
+        private List<User> _usersList = new List<User>()
+        {
+            new User() {userId = 1, userName = "Superman", userEmail = "Superman@google.com"},
+            new User() {userId = 2, userName = "Spiderman", userEmail = "Spiderman@google.com"},
+            new User() {userId = 3, userName = "Batman", userEmail = "Batman@google.com"}
+        };
+
+
+        public UsersController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+
+        #region HttpGet
+
+        //GET api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _usersList;
+        }
+
+        [HttpGet]
+        public User GetUserByID(int id)
+        {
+            string idParam = HttpContext.Current.Request.QueryString["id"];
+
+            User u = _usersList.FirstOrDefault(user => user.userId == id);
+            if(u ==null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return u;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUserByName(string username)
+        {
+            string userNameParam = HttpContext.Current.Request.QueryString["userName"];
+
+            return _usersList.Where(p => string.Equals(p.userName, username, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUserByNameId(string username, int id)
+        {
+            string idParam = HttpContext.Current.Request.QueryString["id"];
+            string userNameParam = HttpContext.Current.Request.QueryString["userName"];
+
+            return _usersList.Where(p => string.Equals(p.userName, username, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUserByModel([FromUri]User user)
+        {
+            string idParam = HttpContext.Current.Request.QueryString["id"];
+            string userNameParam = HttpContext.Current.Request.QueryString["userName"];
+            string email = HttpContext.Current.Request.QueryString["email"];
+
+            return _usersList;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUserByModelSerialize(string userstring)
+        {
+            User user = JsonConvert.DeserializeObject<User>(userstring);
+            return _usersList;
+        }
+
+        public IEnumerable<User> GetUserByModelSerializeWithoutGet(string userstring)
+        {
+            User user = JsonConvert.DeserializeObject<User>(userstring);
+            return _usersList;
+        }
+        //not begin with Get and no attribute([HttpGet]/[HttpPost]/[HttpPut]/[HttpDelete]), will return 405, not sure which request.
+        public IEnumerable<User> NoGetUserByModelSerializeWithoutGet(string userstring)
+        {
+            User user = JsonConvert.DeserializeObject<User>(userstring);
+            return _usersList;
+        }
+
+        #endregion
+
+
+
+
+
+
+    }
+}
