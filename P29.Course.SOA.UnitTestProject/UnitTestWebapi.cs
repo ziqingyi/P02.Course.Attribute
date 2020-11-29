8f5f04df7b14529211e585663c27120beeea04fc
adrian_sun <ziqingyi@qq.com>
2020-11-29 11:18:03 +1100

    use ticket to go through validation and  get response

--- P29.Course.SOA.UnitTestProject/UnitTestWebapi.cs
+++ P29.Course.SOA.UnitTestProject/UnitTestWebapi.cs
@@ -439,18 +439,42 @@ namespace P29.Course.SOA.UnitTestProject
                         .Replace("\"{\\\"Result\\\":true,\\\"Ticket\\\":\\\"", "").Replace("\\\"}\"", "");
 
                     //string ticket2 = JsonHelper
-
                 }
+            }
+            {
+                string loginUrl = "https://localhost:44355/api/users/GetUserByName?username=superman";
+                HttpWebRequest request = (HttpWebRequest) WebRequest.Create(loginUrl);
+                request.Timeout = 300 * 1000;
+                request.Headers.Add(HttpRequestHeader.Authorization, "BasicAuth " + ticket);
+                //request.UserAgent = "";
+                //request.ContentType = "";
+                string result = "";
+                using (HttpWebResponse res = request.GetResponse() as HttpWebResponse)
+                {
+                    if (res.StatusCode == HttpStatusCode.OK)
+                    {
+                        StreamReader reader = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+                        result = reader.ReadToEnd();
+                    }
+                }
+            }
 
 
+            {
+                string loginUrl = "https://localhost:44355/api/users/GetUserByName?username=superman";
+                HttpClientHandler handler = new HttpClientHandler();
 
+                using (HttpClient http = new HttpClient(handler))
+                {
+                    http.DefaultRequestHeaders.Add("Authorization","BasicAuth "+ ticket);
+                    HttpResponseMessage response = http.GetAsync(loginUrl).Result;
+                    Console.WriteLine("Status Code is: " + response.StatusCode);
+                    string result = response.Content.ReadAsStringAsync().Result;
+                }
             }
 
 
         }
-        
-
-
         #endregion
 
 
