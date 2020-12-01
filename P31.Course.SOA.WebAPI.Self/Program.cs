f811dbd981a8a5b031efdc922b82c285c0a9dadc
adrian_sun <ziqingyi@qq.com>
2020-12-02 09:24:03 +1100

    Web API Self Host

---
+++ P31.Course.SOA.WebAPI.Self/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace P31.Course.SOA.WebAPI.Self
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:7077");
+                config.Routes.MapHttpRoute("DefaultApi",
+                    "api/{controller}/{id}",
+                    new {id = RouteParameter.Optional});
+
+                using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+                {
+                    server.OpenAsync().Wait();
+                    Console.WriteLine("Server started.....");
+                    Console.WriteLine("Type in any character..");
+                    Console.Read();
+                    server.CloseAsync().Wait();
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+
+    }
+}
---
+++ P31.Course.SOA.WebAPI.Self/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace P31.Course.SOA.WebAPI.Self
+{
+    public class TestController:ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public string Get(int id)
+        {
+            return "value Get";
+        }
+
+    }
+}
