ca0c61434b6c93e7450df14ad883ff0684c92e9a
adrian_sun <ziqingyi@qq.com>
2020-12-03 17:33:50 +1100

    try aop

---
+++ P31.Course.SOA.WebAPI/Utility/AOP/LogAfterBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.PolicyInjection.Pipeline;
+
+namespace P31.Course.SOA.WebAPI.Utility.AOP
+{
+    public class LogAfterBehavior: IInterceptionBehavior
+    {
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+
+            IMethodReturn methodReturn = getNext()(input, getNext);
+
+            Console.WriteLine("Log After Behavior");
+            foreach (var item in input.Inputs)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("Log After Behavior "+ methodReturn.ReturnValue);
+
+            return methodReturn;
+        }
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+
+    }
+}
---
+++ P31.Course.SOA.WebAPI/Utility/AOP/LogBeforeBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.PolicyInjection.Pipeline;
+
+namespace P31.Course.SOA.WebAPI.Utility.AOP
+{
+    public class LogBeforeBehavior:IInterceptionBehavior
+    {
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            Console.WriteLine("Log Before Behavior");
+            foreach (var item in input.Inputs)// out put all parameters
+            {
+                Console.WriteLine("Logging inputs: {0} , method name: {1}",
+                    item.ToString(), input.MethodBase.Name);//reflection? 
+
+                var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(item);
+
+                Console.WriteLine("Log with Json serialize: {0}", jsonString);
+            }
+
+            return getNext().Invoke(input, getNext);// getNext() return a delegate, then invoke()
+
+        }
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+
+    }
+}
