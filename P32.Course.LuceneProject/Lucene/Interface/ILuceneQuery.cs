cb5f2e5c197271c69aee8ac68c99d19582b19a24
adrian_sun <ziqingyi@qq.com>
2020-12-11 17:31:40 +1100

    interface ILuceneQuery

---
+++ P32.Course.LuceneProject/Lucene/Interface/ILuceneQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P32.Course.LuceneProject.Model;
+
+namespace P32.Course.LuceneProject.Lucene.Interface
+{
+    public interface ILuceneQuery
+    {
+        /// <summary>
+        /// get commodity from query string
+        /// </summary>
+        /// <param name="queryString"></param>
+        /// <returns></returns>
+        List<Commodity> QueryIndex(string queryString);
+
+        /// <summary>
+        /// get commodity data by page index
+        /// </summary>
+        /// <param name="queryString"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="totalCount"></param>
+        /// <param name="priceFilter"></param>
+        /// <param name="priceOrderBy"></param>
+        /// <returns></returns>
+        List<Commodity> QueryIndexPage(string queryString, int pageIndex, int pageSize, out int totalCount,
+            string priceFilter, string priceOrderBy);
+    }
+}
