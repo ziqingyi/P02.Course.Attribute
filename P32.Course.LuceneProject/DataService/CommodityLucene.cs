a2aef8640656ce650ac969e31452b113dde58486
adrian_sun <ziqingyi@qq.com>
2020-12-16 14:08:11 +1100

    commodity lucene

---
+++ P32.Course.LuceneProject/DataService/CommodityLucene.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P32.Course.LuceneProject.Lucene.Interface;
+using P32.Course.LuceneProject.Lucene.Service;
+using P32.Course.LuceneProject.Model;
+using P32.Course.LuceneProject.Utility;
+
+namespace P32.Course.LuceneProject.DataService
+{
+    public class CommodityLucene
+    {
+        private static Logger logger = new Logger(typeof(CommodityLucene));
+
+        #region query commodity
+
+        public static List<Commodity> QueryCommodity(int pageIndex, int pageSize, out int totalCount, string keyword, List<int> categoryIdList, string priceFilter, string priceOrderBy)
+        {
+            totalCount = 0;
+            try
+            {
+                if (string.IsNullOrWhiteSpace(keyword) && (categoryIdList == null || categoryIdList.Count == 0)) return null;
+                ILuceneQuery luceneQuery = new LuceneQuery();
+                string queryString = string.Format(" {0} {1}",
+                    string.IsNullOrWhiteSpace(keyword) ? "" : string.Format(" +{0}", AnalyzerKeyword(keyword)),
+                    categoryIdList == null || categoryIdList.Count == 0 ? "" : string.Format(" +{0}", AnalyzerCategory(categoryIdList)));
+
+                return luceneQuery.QueryIndexPage(queryString, pageIndex, pageSize, out totalCount, priceFilter, priceOrderBy);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(string.Format("QueryCommodity with keyword: {0} has exception", keyword), ex);
+                return null;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// analyze key word using LuceneAnalyze(Pan gu)
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
+        private static string AnalyzerKeyword(string keyword)
+        {
+            StringBuilder queryStringBuilder = new StringBuilder();
+            ILuceneAnalyze analyzer = new LuceneAnalyze();
+            string[] words = analyzer.AnalyzerKey(keyword);
+            if (words.Length == 1)
+            {
+                queryStringBuilder.AppendFormat("{0}:{1}* ", "title", words[0]);
+            }
+            else
+            {
+                StringBuilder fieldQueryStringBuilder = new StringBuilder();
+                foreach (string word in words)
+                {
+                    queryStringBuilder.AppendFormat("{0}:{1} ", "title", word);
+                }
+            }
+            string result = queryStringBuilder.ToString().TrimEnd();
+            logger.Info(string.Format("AnalyzerKeyword 将 keyword={0}转换为{1}", keyword, result));
+            return result;
+        }
+
+
+        /// <summary>
+        /// analyze by category
+        /// </summary>
+        /// <param name="categoryIdList"></param>
+        /// <returns></returns>
+        private static string AnalyzerCategory(List<int> categoryIdList)
+        {
+            return string.Join(" ", categoryIdList.Select(c => string.Format("{0}:{1}", "categoryid", c)));
+        }
+
+
+    }
+}
--- P32.Course.LuceneProject/Program.cs
+++ P32.Course.LuceneProject/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using P32.Course.LuceneProject.DataService;
+using P32.Course.LuceneProject.Model;
+using P32.Course.LuceneProject.Processor;
 
 namespace P32.Course.LuceneProject
 {
@@ -15,6 +18,26 @@ namespace P32.Course.LuceneProject
 
             Console.WriteLine("*************Lucene Show*********************");
             LuceneTest.Show();
+
+            #region Lucene Project
+            IndexBuilder.Build();
+
+
+
+            int total = 0;
+            string pricefilter = "[50,2000]";
+            string priceorderby = "price desc";
+            List<Commodity> commoditylist = CommodityLucene.QueryCommodity(1, 30, out total, "book", null, pricefilter, priceorderby);
+
+            foreach (Commodity commodity in commoditylist)
+            {
+                Console.WriteLine("title={0},price={1}", commodity.Title, commodity.Price);
+            }
+
+
+            #endregion
+
+
 
 
         }
