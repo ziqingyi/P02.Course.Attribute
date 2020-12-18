1cb44a4c1f9c5e46168591cc24375ad0a4084861
adrian_sun <ziqingyi@qq.com>
2020-12-18 14:42:17 +1100

    test change

--- P33.Course.DBFirst/Program.cs
+++ P33.Course.DBFirst/Program.cs
@@ -10,6 +10,59 @@ namespace P33.Course.DBFirst
     {
         static void Main(string[] args)
         {
+
+            try
+            {
+                using (advanced7Entities context = new advanced7Entities())
+                {
+                    context.Database.Log += s => Console.WriteLine($"current running sql: {s}");
+
+
+                    User user17= context.Users.Find(17);
+
+                    User user28 = context.Users.FirstOrDefault(u => u.Id == 28);
+
+                    User userNew = new User()
+                    {
+                        Account = "Admin",
+                        State = 0,
+                        CompanyId = 4,
+                        CompanyName = "CoCo",
+                        CreateTime = DateTime.Now,
+                        CreatorId = 1,
+                        Email = "57265177@gmail.com",
+                        LastLoginTime = null,
+                        LastModifierId = 0,
+                        LastModifyTime = DateTime.Now,
+                        Mobile = "0256813881",
+                        Name = "yoyo",
+                        Password = "12356789",
+                        UserType = 1
+                    };
+
+                    //add
+                    context.Users.Add(userNew);
+                    context.SaveChanges();//create a transaction and send query to db.
+
+                    //change, change in object will go to database
+                    userNew.Name = "CodeMan";
+                    context.SaveChanges();
+
+                    //remove
+                    context.Users.Remove(userNew);
+                    context.SaveChanges();
+
+
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
