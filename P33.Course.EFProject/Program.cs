9c2ec784558bf7ad6328ab9e6c5d87e8aebff4f0
adrian_sun <ziqingyi@qq.com>
2020-12-21 09:14:35 +1100

    console project using EF Model

---
+++ P33.Course.EFProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P33.Course.Model;
+using P33.Course.Model.Models;
+
+namespace P33.Course.EFProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //must reference EF in current project for implementation of disposing. 
+            //must add connection string from model,as JDDbContext will use current proj's config. 
+            using (JDDbContext context = new JDDbContext())
+            {
+                User user = context.Users.Find(2);
+
+
+            }
+
+
+        }
+    }
+}
