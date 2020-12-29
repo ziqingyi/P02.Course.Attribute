c767a791af5cfe28f690cfe7f5b91f10c4194265
adrian_sun <ziqingyi@qq.com>
2020-12-29 15:45:12 +1100

    EF IOC structure

---
+++ P34.Course.EF_IOC_Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P34.Course.EF_IOC_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IOCTest.Show();
+
+        }
+    }
+}
