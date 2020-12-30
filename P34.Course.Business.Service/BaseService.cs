9d60f2897d09e298796cf4052e04920ce26cdace
adrian_sun <ziqingyi@qq.com>
2020-12-30 13:25:45 +1100

    config, package and base service query

--- P34.Course.Business.Service/BaseService.cs
+++ P34.Course.Business.Service/BaseService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using P34.Course.Business.Interface;
@@ -10,12 +13,108 @@ namespace P34.Course.Business.Service
     public class BaseService: IBaseService
     {
         //use base service to do all the service for different objects(tables), use T
+        #region Identity
+        //protected: only sub class can use. private: only base can set context value
+        protected DbContext Context { get; private set; }
+        /// <summary>
+        /// share DbContext in one request, use one in a BaseService instance to curd.
+        /// </summary>
+        /// <param name="context"></param>
+        public BaseService(DbContext context)//use super class: DbContext
+        {
+            this.Context = context;
+        }
+        #endregion
+
+        #region Query
+
+        public T Find<T>(int id) where T : class
+        {
+            return this.Context.Set<T>().Find(id);
+        }
+
+        [Obsolete("avoid using this,better to use query with Expression tree, using(...)")]
+        public IQueryable<T> Set<T>() where T : class
+        {
+            return this.Context.Set<T>();
+        }
+
+        public IQueryable<T> Query<T>(Expression<Func<T, bool>> funcWhere) where T : class
+        {
+            return this.Context.Set<T>().Where<T>(funcWhere);
+        }
+
+        public PageResult<T> QueryPage<T, S>(Expression<Func<T, bool>> funcWhere, int pageSize, int pageIndex,
+            Expression<Func<T, S>> funcOrderby, bool isAsc = true) where T : class
+        {
+            var list = this.Set<T>();
+            if (funcWhere != null)
+            {
+                list = list.Where<T>(funcWhere);
+            }
+
+            if (isAsc)
+            {
+                list = list.OrderBy(funcOrderby);
+            }
+            else
+            {
+                list = list.OrderByDescending(funcOrderby);
+            }
+            PageResult<T> result = new PageResult<T>()
+            {
+                DataList =  list.Skip( (pageIndex-1)* pageSize ).Take(pageSize).ToList(),
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                TotalCount = this.Context.Set<T>().Count(funcWhere)
+            };
+            return result;
+        }
+        #endregion
 
 
+
+
+
+        #region other
+
+        public void Commit()
+        {
+            this.Context.SaveChanges();
+        }
+
+        public IQueryable<T> ExecuteQuery<T>(string sql, SqlParameter[] parameters) where T : class
+        {
+            return this.Context.Database.SqlQuery<T>(sql, parameters).AsQueryable();
+        }
+
+        public void Execute<T>(string sql, SqlParameter[] parameters) where T : class
+        {
+            DbContextTransaction trans = null;
+            try
+            {
+                trans = this.Context.Database.BeginTransaction();
+                this.Context.Database.ExecuteSqlCommand(sql, parameters);
+                trans.Commit();
+            }
+            catch (Exception ex)
+            {
+                if (trans != null)
+                {
+                    trans.Rollback();
+                }
+                throw ex;
+            }
+        }
 
         public void Dispose()
         {
-            
+            if (this.Context != null)
+            {
+                this.Context.Dispose();
+            }
         }
+        #endregion
+
     }
 }
