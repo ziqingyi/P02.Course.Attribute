624e4cff73cb5510e30a7d107e670528d5e49095
adrian_sun <ziqingyi@qq.com>
2020-12-31 21:41:41 +1100

    Ioc test

---
+++ P35.Course.EF_IOC_Test/ContainerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity.Configuration;
+using Unity;
+
+namespace P35.Course.EF_IOC_Test
+{
+    public class ContainerFactory
+    {
+        private static IUnityContainer container = null;
+        static ContainerFactory()
+        {
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+            fileMap.ExeConfigFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "CfgFiles\\Unity.Config");
+            Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            UnityConfigurationSection section = (UnityConfigurationSection)configuration.GetSection(UnityConfigurationSection.SectionName);
+            container = new UnityContainer();
+            section.Configure(container, "myccContainer");
+        }
+
+        public static IUnityContainer GetContainer()
+        {
+            return container;
+        }
+    }
+}
--- P35.Course.EF_IOC_Test/IOCTestCase.cs
+++ P35.Course.EF_IOC_Test/IOCTestCase.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using P33.Course.Model.Models;
+using P34.Course.Business.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
 
 namespace P35.Course.EF_IOC_Test
 {
@@ -10,8 +13,13 @@ namespace P35.Course.EF_IOC_Test
     {
         public static void Show()
         {
+            IUnityContainer container = ContainerFactory.GetContainer();
+            using (IUserCompanyService iUserCompanyService = container.Resolve<IUserCompanyService>())
+            {
+                User u = iUserCompanyService.Find<User>(12);
 
 
+            }
 
 
         }
