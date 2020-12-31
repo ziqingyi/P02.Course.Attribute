6b292b10892f00c105c140462b462c6aff4e173d
adrian_sun <ziqingyi@qq.com>
2020-12-31 15:53:01 +1100

    IOC test project

---
+++ P35.Course.EF_IOC_Test/IOCTestCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P35.Course.EF_IOC_Test
+{
+    public class IOCTestCase
+    {
+        public static void Show()
+        {
+
+
+
+
+        }
+
+
+    }
+}
---
+++ P35.Course.EF_IOC_Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P35.Course.EF_IOC_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOCTestCase.Show();
+        }
+    }
+}
