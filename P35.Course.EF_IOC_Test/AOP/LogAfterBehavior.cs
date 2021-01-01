f028fac4b39f13cd184bb3b27c4e46f4ddc9e734
adrian_sun <ziqingyi@qq.com>
2021-01-01 21:45:27 +1100

    aop config, add log

---
+++ P35.Course.EF_IOC_Test/AOP/LogAfterBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.PolicyInjection.Pipeline;
+
+namespace P35.Course.EF_IOC_Test.AOP
+{
+    public class LogAfterBehavior : IInterceptionBehavior
+    {
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            //transaction can start here
+            IMethodReturn methodReturn = getNext()(input, getNext);
+            //transaction can commit here
+
+
+            Console.WriteLine($"{input.MethodBase.Name} LogAfterBehavior..Start.");
+            foreach (var item in input.Inputs)
+            {
+                Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(item));
+            }
+            Console.WriteLine($"{input.MethodBase.Name} LogAfterBehavior..  End.");
+            return methodReturn;
+        }
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+    }
+}
