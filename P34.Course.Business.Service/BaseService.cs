6cc90b6274fd8af3273f39bd7472e0afa8467444
adrian_sun <ziqingyi@qq.com>
2021-01-01 20:42:24 +1100

    use abstract class

--- P34.Course.Business.Service/BaseService.cs
+++ P34.Course.Business.Service/BaseService.cs
@@ -10,7 +10,8 @@ using P34.Course.Business.Interface;
 
 namespace P34.Course.Business.Service
 {
-    public class BaseService: IBaseService
+    //use abstract class, so this class cannot be used for initialization. 
+    public abstract class BaseService: IBaseService
     {
         //use base service to do all the service for different objects(tables), use T
         #region Identity
