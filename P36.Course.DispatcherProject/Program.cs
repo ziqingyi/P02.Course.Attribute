1b878c0bf987dffd2e74d13684b4ff1ea11fe64f
adrian_sun <ziqingyi@qq.com>
2021-01-02 21:19:24 +1100

    quartz project structure

---
+++ P36.Course.DispatcherProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P36.Course.DispatcherProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
