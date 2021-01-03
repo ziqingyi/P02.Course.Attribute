e53eb7a293dc8945fcd641d8f4e8ce3b1c27b53b
adrian_sun <ziqingyi@qq.com>
2021-01-03 12:42:41 +1100

    scheduler init

--- P36.Course.DispatcherProject.QuartzNet/DispatcherManager.cs
+++ P36.Course.DispatcherProject.QuartzNet/DispatcherManager.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quartz;
 
 namespace P36.Course.DispatcherProject.QuartzNet
 {
     public class DispatcherManager
     {
+        //1 nuget
+        //2 three core object:
+        //      (1) IScheduler -- config scheduler task.
+        //                        it's a container and should start for tasks inside. 
+        //      (2) IJob: task, which will be executed
+        //      (3) ITrigger: properties needed to instantiate an actual Trigger.
+
+        public async static void Init()
+        {
+            #region scheduler
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+            await scheduler.Start();
+            #endregion
+
+
+
+
+
+        }
+
 
 
 
