a0f49aae24c140f6848472123389a3923a910a98
adrian_sun <ziqingyi@qq.com>
2021-01-11 09:09:54 +1100

    schedule manager

---
+++ P36.Course.DispatcherProject.QuartzNet/ScheduleManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace P36.Course.DispatcherProject.QuartzNet
+{
+    public class ScheduleManager
+    {
+        public async static Task<IScheduler> BuildScheduler()
+        {
+            var properties = new NameValueCollection();
+            properties["quartz.scheduler.instanceName"] = "jobs monitoring system";
+
+            // thread pool setting
+            properties["quartz.threadPool.type"] = "Quartz.Simpl.SimpleThreadPool, Quartz";
+            properties["quartz.threadPool.threadCount"] = "5";
+            properties["quartz.threadPool.threadPriority"] = "Normal";
+
+            // exporter setting,  port is web.config SchedulerHost port number. 
+            properties["quartz.scheduler.exporter.type"] = "Quartz.Simpl.RemotingSchedulerExporter, Quartz";
+            properties["quartz.scheduler.exporter.port"] = "8008";
+            properties["quartz.scheduler.exporter.bindName"] = "QuartzScheduler";
+            properties["quartz.scheduler.exporter.channelType"] = "tcp";
+
+            var schedulerFactory = new StdSchedulerFactory(properties);
+            IScheduler _scheduler = await schedulerFactory.GetScheduler();
+            return _scheduler;
+        }
+
+    }
+
+
+}
