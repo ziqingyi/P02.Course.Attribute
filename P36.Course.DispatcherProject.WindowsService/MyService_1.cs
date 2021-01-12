5c1851f9ea6723841ba701f52fc37652a283b2e5
adrian_sun <ziqingyi@qq.com>
2021-01-13 08:42:56 +1100

    update service name, quartz package

--- P36.Course.DispatcherProject.WindowsService/MyService_1.cs
+++ P36.Course.DispatcherProject.WindowsService/MyService_1.cs
@@ -11,10 +11,10 @@ using P36.Course.DispatcherProject.Common;
 
 namespace P36.Course.DispatcherProject.WindowsService
 {
-    public partial class Service1 : ServiceBase
+    public partial class MyService_1 : ServiceBase
     {
-        private Logger logger = new Logger(typeof(Service1));
-        public Service1()
+        private Logger logger = new Logger(typeof(MyService_1));
+        public MyService_1()
         {
             InitializeComponent();
         }
--- P36.Course.DispatcherProject.WindowsService/Program.cs
+++ P36.Course.DispatcherProject.WindowsService/Program.cs
@@ -19,7 +19,7 @@ namespace P36.Course.DispatcherProject.WindowsService
                 ServiceBase[] ServicesToRun;
                 ServicesToRun = new ServiceBase[]
                 {
-                    new Service1()
+                    new MyService_1()
                 };
                 ServiceBase.Run(ServicesToRun);
             }
