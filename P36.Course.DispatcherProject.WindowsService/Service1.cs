60f68eb52e35e2e5788a7634f69143211bb149b1
adrian_sun <ziqingyi@qq.com>
2021-01-12 15:42:55 +1100

    logger common dll and installer

--- P36.Course.DispatcherProject.WindowsService/Service1.cs
+++ P36.Course.DispatcherProject.WindowsService/Service1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using P36.Course.DispatcherProject.Common;
 
 namespace P36.Course.DispatcherProject.WindowsService
 {
     public partial class Service1 : ServiceBase
     {
+        private Logger logger = new Logger(typeof(Service1));
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +21,13 @@ namespace P36.Course.DispatcherProject.WindowsService
 
         protected override void OnStart(string[] args)
         {
+            this.logger.Info("This is OnStart..");
+
         }
 
         protected override void OnStop()
         {
+            this.logger.Info("This is OnStop..");
         }
     }
 }
