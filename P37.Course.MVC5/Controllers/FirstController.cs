17b9a0b89d340c15e25c5aa05067c1efb90f2ea0
adrian_sun <ziqingyi@qq.com>
2021-01-14 19:58:04 +1100

    first controller test views

---
+++ P37.Course.MVC5/Controllers/FirstController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        ///https://localhost:44332/First/IndexId/3  id is directed by router. only id can do this way.
+        /// https://localhost:44332/First/IndexId?id=3 url address pass param. 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ViewResult IndexId(int id)
+        {
+            return View();
+        }
+
+        public ViewResult IndexIdNull(int? id)
+        {
+            return View();
+        }
+
+    }
+}
