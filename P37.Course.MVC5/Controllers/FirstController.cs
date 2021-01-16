ae27b5be0a36712524c61df6a91196b6909f390f
adrian_sun <ziqingyi@qq.com>
2021-01-16 12:55:33 +1100

    test data, ViewBag ViewData

--- P37.Course.MVC5/Controllers/FirstController.cs
+++ P37.Course.MVC5/Controllers/FirstController.cs
@@ -3,16 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using P37.Course.MVC5.Models;
 
 namespace P37.Course.MVC5.Controllers
 {
     public class FirstController : Controller
     {
-        // GET: First
-        public ActionResult Index()
+        #region User Data
+        private List<CurrentUser> _UserList = new List<CurrentUser>()
         {
-            return View();
+            new CurrentUser()
+            {
+                Id=1,
+                Name="User1",
+                Account="Administrator",
+                Email="111111@qq.com",
+                LoginTime=DateTime.Now,
+                Password="123drd",
+                Datas = new List<Data>()
+                {
+                    new Data(){Company = "company1"},
+                    new Data(){Company = "company_a"}
+                }
+            },
+            new CurrentUser()
+            {
+                Id=2,
+                Name="User2",
+                Account="Administrator",
+                Email="222222@qq.com",
+                LoginTime=DateTime.Now.AddMonths(-2),
+                Password="123ljlji",
+                Datas = new List<Data>()
+                {
+                    new Data(){Company = "company2"},
+                    new Data(){Company = "company_b"}
+                }
+            },
+            new CurrentUser()
+            {
+                Id=3,
+                Name="User3",
+                Account="Administrator",
+                Email="33333@qq.com",
+                LoginTime=DateTime.Now.AddDays(-5),
+                Password="1ljkll56",
+                Datas = new List<Data>()
+                {
+                    new Data(){Company = "company3"},
+                    new Data(){Company = "company_c"}
+                }
+            },
+        };
+        #endregion
+        ////ambitious
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: First
+        public ActionResult Index(int id = 3)
+        {
+            CurrentUser currentUser = this._UserList.FirstOrDefault(u=> u.Id == id)??this._UserList[0];
+
+
+            base.ViewBag.CurrentUserViewBag = this._UserList[1];//ViewBag is dynamic type
+            base.ViewBag.TestProp = "view bag test prop value";//override by view data,because view data execute late
+
+
+            base.ViewData["CurrentUserViewData"] = this._UserList[0];
+            base.ViewData["TestProp"] = "view data test prop value";
+
+
+            return View(this._UserList[2]);
+            //return View("~/Views/First/Index.cshtml");//return cshtml view only
         }
+
 
         //https://localhost:44332/First/IndexId/3  id is directed by router. only id can do this way.
         // https://localhost:44332/First/IndexId?id=3 url address pass param. 
---
+++ P37.Course.MVC5/Models/CurrentUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace P37.Course.MVC5.Models
+{
+    public class CurrentUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        public DateTime LoginTime { get; set; }
+
+        public List<Data> Datas { get; set; }
+    }
+
+    public class Data
+    {
+        public string Company { get; set; }
+    }
+}
