47d7f172e3467cced09f6f8a692ebaa05a6dca8d
adrian_sun <ziqingyi@qq.com>
2021-01-16 19:12:25 +1100

    logger

--- P37.Course.MVC5/Global.asax.cs
+++ P37.Course.MVC5/Global.asax.cs
@@ -5,17 +5,21 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using P37.Course.MVC5.Controllers.Utility;
 
 namespace P37.Course.MVC5
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private Logger logger = new Logger(typeof(MvcApplication));
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            this.logger.Info("website start.....");
         }
     }
 }
---
+++ P37.Course.MVC5/Utility/Logger.cs
@@ -0,0 +1,60 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+
+namespace P37.Course.MVC5.Controllers.Utility
+{
+    public class Logger
+    {
+        static Logger()
+        {
+            FileInfo fi = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CfgFiles\\log4net.config"));
+            XmlConfigurator.Configure(fi);
+
+            ILog Log = LogManager.GetLogger(typeof(Logger));
+            Log.Info("initialise Logger");
+        }
+
+        private ILog loger = null;
+
+        public Logger(Type type)
+        {
+            loger = LogManager.GetLogger(type);
+        }
+
+        public void Error(string msg = "Exception", Exception ex = null)
+        {
+            Console.WriteLine(msg);
+            loger.Error(msg, ex);
+        }
+
+        public void Warn(string msg)
+        {
+            Console.WriteLine(msg);
+            loger.Warn(msg);
+        }
+
+        public void Info(string msg)
+        {
+            Console.WriteLine(msg);
+            loger.Info(msg);
+        }
+
+        public void Debug(string msg)
+        {
+            Console.WriteLine(msg);
+            loger.Debug(msg);
+        }
+
+
+
+
+
+    }
+}
