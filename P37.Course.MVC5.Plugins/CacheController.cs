a3410edc0680632582eac7299510aa5bb67c8fca
adrian_sun <ziqingyi@qq.com>
2021-01-18 14:50:08 +1100

    reference controller from other project

---
+++ P37.Course.MVC5.Plugins/CacheController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Plugins
+{
+    public class CacheController: Controller 
+    {
+        public ActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+
+    }
+}
