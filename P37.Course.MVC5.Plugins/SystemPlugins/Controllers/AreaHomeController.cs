618d8b8d39c072a6994e0dfb2e7bde050b7e315e
adrian_sun <ziqingyi@qq.com>
2021-01-19 12:04:14 +1100

    separate controller and area files

---
+++ P37.Course.MVC5.Plugins/SystemPlugins/Controllers/AreaHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Plugins.SystemPlugins.Controllers
+{
+    public class AreaHomeController : Controller
+    {
+        // GET: System/AreaHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ P37.Course.MVC5.Plugins/SystemPlugins/SystemAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Areas.System
+{
+    public class SystemAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SystemPlugins";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SystemPlugins_default",
+                "SystemPlugins/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
