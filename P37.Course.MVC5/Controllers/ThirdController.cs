e3f37910a4e69b72a5a0d61341e842efde9d5888
adrian_sun <ziqingyi@qq.com>
2021-01-21 17:45:20 +1100

    copy connection strings to web config

--- P37.Course.MVC5/Controllers/ThirdController.cs
+++ P37.Course.MVC5/Controllers/ThirdController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using P33.Course.Model;
 using P33.Course.Model.Models;
 using P34.Course.Business.Interface;
 using P34.Course.Business.Service;
@@ -14,8 +15,9 @@ namespace P37.Course.MVC5.Controllers
         // GET: Third
         public ActionResult Index()
         {
-            IUserService service = new UserService();
-            User user =service.Find<User>(2)
+            JDDbContext context = new JDDbContext();
+            IUserService service = new UserService(context);
+            User user = service.Find<User>(2);
 
             return View();
         }
