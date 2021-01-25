4a0654777bf7d1e53daa92bb66cc39ed998f0194
adrian_sun <ziqingyi@qq.com>
2021-01-25 14:19:57 +1100

    fourth controller

---
+++ P37.Course.MVC5/Controllers/FourthController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Controllers
+{
+    public class FourthController : Controller
+    {
+
+
+
+
+
+
+        // GET: Fourth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
