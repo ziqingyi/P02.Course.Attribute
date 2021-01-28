8cbbef2433d09035a826f2487f9020ee8a758c44
adrian_sun <ziqingyi@qq.com>
2021-01-28 22:35:50 +1100

    WebApi packages and Web Api Config

---
+++ P37.Course.MVC5/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace P37.Course.MVC5.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //Web Api Config and service
+
+
+            //Web Api route
+            config.MapHttpAttributeRoutes();
+
+
+            config.Routes.MapHttpRoute(
+                name:"DefaultApi",
+                routeTemplate:"api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+            );
+
+        }
+
+    }
+}
--- P37.Course.MVC5/Global.asax.cs
+++ P37.Course.MVC5/Global.asax.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using P37.Course.MVC5.App_Start;
 using P37.Course.MVC5.Controllers.Utility;
 using P37.Course.MVC5.Utility;
+//using WebGrease.Configuration;
 
 namespace P37.Course.MVC5
 {
@@ -16,6 +19,7 @@ namespace P37.Course.MVC5
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
