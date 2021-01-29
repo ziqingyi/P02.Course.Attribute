f73c81c51ac9f04b8e00948018725d338d72963f
adrian_sun <ziqingyi@qq.com>
2021-01-29 11:05:18 +1100

    add new MyArea

---
+++ P37.Course.MVC5/Areas/MyArea/MyAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Areas.MyArea
+{
+    public class MyAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MyArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "MyArea_default",
+                "MyArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
