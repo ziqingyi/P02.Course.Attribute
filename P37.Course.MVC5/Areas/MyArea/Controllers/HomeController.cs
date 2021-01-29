20b0293e01a2a28c5331a0987bcbb73418fd3d1e
adrian_sun <ziqingyi@qq.com>
2021-01-29 11:06:35 +1100

    add home controller for MyArea

---
+++ P37.Course.MVC5/Areas/MyArea/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Areas.MyArea.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: MyArea/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
