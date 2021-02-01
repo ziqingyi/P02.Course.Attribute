e8086a7005434619ac4e56f8469a819dafbdbf4b
adrian_sun <ziqingyi@qq.com>
2021-02-01 15:19:55 +1100

    controller  t with service

--- P37.Course.MVC5/Controllers/FourthController.cs
+++ P37.Course.MVC5/Controllers/FourthController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using P34.Course.Business.Interface;
 
 namespace P37.Course.MVC5.Controllers
 {
     public class FourthController : Controller
     {
-
-
-
+        private ICommodityService _commodityService = null;
+        public FourthController(ICommodityService commodityService)
+        {
+            this._commodityService = commodityService;
+        }
 
 
 
