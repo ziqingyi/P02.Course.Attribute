4f911acc2ba484e84ac161467a451f6d71169356
adrian_sun <ziqingyi@qq.com>
2021-02-01 15:15:10 +1100

    commodity service and interface

---
+++ P34.Course.Business.Service/CommodityService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P34.Course.Business.Interface;
+
+namespace P34.Course.Business.Service
+{
+    public class CommodityService:BaseService, IBaseService
+    {
+        public CommodityService(DbContext context) : base(context)
+        {
+
+        }
+
+    }
+}
