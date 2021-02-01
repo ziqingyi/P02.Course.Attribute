e76316dabf41b641a1d2d45b62a5b80985a29bff
adrian_sun <ziqingyi@qq.com>
2021-02-02 08:54:38 +1100

    search by search string

--- P37.Course.MVC5/Controllers/FourthController.cs
+++ P37.Course.MVC5/Controllers/FourthController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using P33.Course.Model.Models;
 using P34.Course.Business.Interface;
 
@@ -21,8 +23,17 @@ namespace P37.Course.MVC5.Controllers
         // GET: Fourth
         public ActionResult Index()
         {
+            string searchString = base.HttpContext.Request.Form["searchString"];
+            Expression<Func<JD_Commodity_001, bool>> funcWhere = null;
+            var commodityList = this._commodityService.Query<JD_Commodity_001>(c => c.Id < 2);
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                funcWhere = c => c.Title.Contains(searchString);
+                commodityList = this._commodityService.Query<JD_Commodity_001>(funcWhere);
 
-            var commodityList = this._commodityService.Query<JD_Commodity_001>(c => c.Id < 100);
+                base.ViewBag.SearchString = searchString;
+            }
+           
 
             return View(commodityList);
         }
