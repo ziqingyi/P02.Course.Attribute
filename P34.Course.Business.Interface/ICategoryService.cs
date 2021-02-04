9efbe4959062ff6f3c1fca1c7a1ff8b91dfd81b6
adrian_sun <ziqingyi@qq.com>
2021-02-05 09:07:38 +1100

    ICategoryService

---
+++ P34.Course.Business.Interface/ICategoryService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P33.Course.Model.Models;
+
+namespace P34.Course.Business.Interface
+{
+    public interface ICategoryService:IBaseService
+    {
+        void Show();
+
+        #region Query
+        /// <summary>
+        /// get all id in current category and descendant category
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        IEnumerable<int> GetDescendantsIdList(string code);
+
+        /// <summary>
+        /// get child list, default is root
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        IEnumerable<Category> GetChildList(string code = "root");
+
+
+        /// <summary>
+        /// catch all categories, no change in most cases.
+        /// </summary>
+        /// <returns></returns>
+        List<Category> CacheAllCategory();
+
+        #endregion
+
+
+
+    }
+}
