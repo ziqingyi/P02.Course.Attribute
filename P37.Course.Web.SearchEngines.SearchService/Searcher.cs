c7a17c31bc1f72fb5bde274796679fb0d8e33eff
adrian_sun <ziqingyi@qq.com>
2021-02-05 12:55:21 +1100

    searcher

--- P37.Course.Web.SearchEngines.SearchService/Searcher.cs
+++ P37.Course.Web.SearchEngines.SearchService/Searcher.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using P37.Course.Web.SearchEngines.SearchService.AOP;
 
 namespace P37.Course.Web.SearchEngines.SearchService
 {
     public class Searcher: ISearcher
     {
 
-
+        public string QueryCommodityPage(int pageIndex, int pageSize, string keyword, List<int> categoryIdList, string priceFilter, string priceOrderBy)
+        {
+            ISearcherAOP searcher = SearcherAOPFactory.CreateInstance();
+            return searcher.QueryCommodityPage(pageIndex, pageSize, keyword, categoryIdList, priceFilter, priceOrderBy);
+        }
 
 
     }
