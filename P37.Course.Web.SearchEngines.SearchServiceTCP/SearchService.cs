eae86374313ff69f9a880b8708ab0060bb1115ba
adrian_sun <ziqingyi@qq.com>
2021-02-08 10:33:35 +1100

    SearchServiceTCP proj

---
+++ P37.Course.Web.SearchEngines.SearchServiceTCP/SearchService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P37.Course.Web.SearchEngines.SearchServiceTCP
+{
+    public class SearchService: ISearchService
+    {
+    }
+}
