0a60238e324a794b9ad1d5c2063ea88e08965984
adrian_sun <ziqingyi@qq.com>
2021-02-08 11:07:26 +1100

    search service

--- P37.Course.Web.SearchEngines.SearchServiceTCP/SearchService.cs
+++ P37.Course.Web.SearchEngines.SearchServiceTCP/SearchService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using P34.Course.Business.Interface;
+using P37.Course.Web.SearchEngines.Interface;
+using P37.Course.Web.SearchEngines.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,34 @@ namespace P37.Course.Web.SearchEngines.SearchServiceTCP
 {
     public class SearchService: ISearchService
     {
+        public PageResult<CommodityModel> QueryCommodityPage(int pageIndex, int pageSize, string keyword,
+            List<int> categoryIdList, string priceFilter, string priceOrderBy)
+        {
+
+
+            try
+            {
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                
+
+
+
+            }
+
+
+
+
+
+
+
+        }
+
+
     }
 }
