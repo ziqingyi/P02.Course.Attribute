c7048579de55ed2842691c3a04ad36c7e162224f
adrian_sun <ziqingyi@qq.com>
2021-02-26 17:18:39 +1100

    add mvc controller

---
+++ P37.Course.MVC5/Controllers/MVCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Controllers
+{
+    public class MVCController : Controller
+    {
+        // GET: MVC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
