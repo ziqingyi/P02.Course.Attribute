4e6edc7f3985d7e934c5b1d6fb6532d0d9b59273
adrian_sun <ziqingyi@qq.com>
2021-03-10 21:15:41 +1100

    paing page

---
+++ P37.Course.MVC5/Controllers/PagingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Controllers
+{
+    public class PagingController : Controller
+    {
+        // GET: Paging
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
