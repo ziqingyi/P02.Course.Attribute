b73a32651c5788ae3b54e0925c3f4c36f354fd9a
adrian_sun <ziqingyi@qq.com>
2021-03-14 11:54:25 +1100

    crud and batch

--- P33.Course.DBFirst/Program.cs
+++ P33.Course.DBFirst/Program.cs
@@ -10,7 +10,50 @@ namespace P33.Course.DBFirst
     {
         static void Main(string[] args)
         {
+            BatchTest();
 
+            CRUDtests();
+            
+        }
+
+        public static void BatchTest()
+        {
+            using (advanced7Entities contextEntity = new advanced7Entities())
+            {
+                contextEntity.Database.Log += s => Console.WriteLine($"current running sql: {s}");
+
+                #region Batch
+
+                List<User> list = contextEntity.Users
+                    .Where(m => m.UserType == 2)
+                    .OrderByDescending(m=>m.Id)
+                    .ToList();
+                foreach (User u in list)
+                {
+                    u.UserType = u.Id % 3;
+                }
+
+                if (contextEntity.SaveChanges()>0)
+                {
+                    Console.WriteLine("update successfully");
+                }
+
+
+                #endregion
+
+
+
+
+
+
+            }
+
+
+        }
+
+
+        public static void CRUDtests()
+        {
             try
             {
                 using (advanced7Entities context = new advanced7Entities())
@@ -18,7 +61,7 @@ namespace P33.Course.DBFirst
                     context.Database.Log += s => Console.WriteLine($"current running sql: {s}");
 
                     #region Search
-                    User user17= context.Users.Find(17);
+                    User user17 = context.Users.Find(17);
                     User user28 = context.Users.FirstOrDefault(u => u.Id == 28);
                     #endregion
 
@@ -43,10 +86,14 @@ namespace P33.Course.DBFirst
                     };
 
                     //add by reference and save
-                    var addu = context.Users.Add(userNew);
-                    
+                    var addU = context.Users.Add(userNew);//id is 0 at now
+
                     var AddRes = context.SaveChanges();//create a transaction and send query to db.
 
+                    if (AddRes > 0)//user id is updated by dbcontext with new value
+                    {
+                        Console.WriteLine("add successfully..");
+                    }
 
                     #endregion
 
@@ -55,9 +102,18 @@ namespace P33.Course.DBFirst
                     #region update
 
                     //change, change in object will go to database
-                    userNew.Name = "CodeMan";
-                    var objChangeRes = context.SaveChanges();
-                    
+                    //search and find the reference first
+
+                    User updateU = context.Users.FirstOrDefault(m => m.Id == userNew.Id);
+
+                    updateU.Name = "CodeMan";
+
+                    var updateRes = context.SaveChanges();
+
+                    if (updateRes > 0)
+                    {
+                        Console.WriteLine("update successfully..");
+                    }
                     #endregion
 
 
@@ -68,10 +124,13 @@ namespace P33.Course.DBFirst
                     //search and find the reference first
                     User uToDelete = context.Users.Single(m => m.Id == userNew.Id);
 
-                    var remRes = context.Users.Remove(uToDelete);
-                    context.SaveChanges();
+                    var remU = context.Users.Remove(uToDelete);
+                    var remRes = context.SaveChanges();
 
-
+                    if (remRes > 0)
+                    {
+                        Console.WriteLine("remove successfully..");
+                    }
                     #endregion
 
 
@@ -85,7 +144,10 @@ namespace P33.Course.DBFirst
                 Console.WriteLine(ex.Message);
             }
 
+
         }
+
+
 
 
 
