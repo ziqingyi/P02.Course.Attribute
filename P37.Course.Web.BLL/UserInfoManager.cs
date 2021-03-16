498037cfbc02f4ea6d3189910237d39c9dc4f17e
adrian_sun <ziqingyi@qq.com>
2021-03-16 15:45:45 +1100

    db first EF to database, BLL with business logic , service using BLL structure

---
+++ P37.Course.Web.BLL/UserInfoManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P37.Course.Web.BLL
+{
+    public class UserInfoManager
+    {
+        // use P37.Course.Web.DAL.EF.DBFirst to create business logic
+
+        public bool AddUser()
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ P37.Course.Web.ServiceEF.DBFirst/UserInfoService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P37.Course.Web.ServiceEF.DBFirst
+{
+    public class UserInfoService
+    {
+        //use P37.Course.Web.BLL 
+
+        UserInfoService userInfoService = new UserInfoService();
+
+        public bool Register()
+        {
+
+
+            return userInfoService.Register();
+        }
+
+
+
+    }
+}
