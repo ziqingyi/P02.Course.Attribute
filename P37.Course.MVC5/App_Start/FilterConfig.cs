5fbf3d5fd2fc05402deff2bb2dabb1e1ee2fd793
adrian_sun <ziqingyi@qq.com>
2021-03-27 11:37:03 +1100

    auth attribute to all controllers

--- P37.Course.MVC5/App_Start/FilterConfig.cs
+++ P37.Course.MVC5/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using P37.Course.Web.Core.Attributes;
 
 namespace P37.Course.MVC5
 {
@@ -8,6 +9,8 @@ namespace P37.Course.MVC5
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //attribute will take effect on all controllers
+            //filters.Add(new CustomAuthorizeAttribute());
         }
     }
 }
