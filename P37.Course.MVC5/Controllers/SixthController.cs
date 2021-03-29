e1479a6cb8f5c89d1e03a9590e681f01f5085c17
adrian_sun <ziqingyi@qq.com>
2021-03-30 08:52:10 +1100

    six controller with index page

---
+++ P37.Course.MVC5/Controllers/SixthController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using P34.Course.Business.Interface;
+using P37.Course.Web.Core.Utility;
+using Unity;
+
+namespace P37.Course.MVC5.Controllers
+{
+    public class SixthController : Controller
+    {
+        #region Identity
+        private Logger logger = new Logger(typeof(SixthController));
+
+        private IUserService _iUserService = null;
+        private ICompanyService _iCompanyService = null;
+        private IUserCompanyService _iUserCompanyService = null;
+
+
+        /// <summary>
+        /// ctor function injection --controller is initialized by container
+        /// </summary>
+        /// <param name="userService"></param>
+        /// <param name="companyService"></param>
+        /// <param name="userCompanyService"></param>
+        [InjectionConstructor]//container will use this ctor
+        public SixthController(IUserService userService, ICompanyService companyService,
+            IUserCompanyService userCompanyService)
+        {
+            this._iUserService = userService;
+            this._iCompanyService = companyService;
+            this._iUserCompanyService = userCompanyService;
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
