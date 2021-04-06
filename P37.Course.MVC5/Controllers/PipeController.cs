426f22c6bc0f785a674419c4420e0129ac0825b5
adrian_sun <ziqingyi@qq.com>
2021-04-06 21:54:18 +1000

    pipe controller

---
+++ P37.Course.MVC5/Controllers/PipeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P37.Course.MVC5.Controllers
+{
+    public class PipeController : Controller
+    {
+        // GET: Pipe
+        public ActionResult Index()
+        {
+            //HttpRuntime.ProcessRequest(null);
+            //HttpApplication
+            return View();
+        }
+
+        #region HttpModule
+
+
+
+        #endregion
+
+
+        #region Http Handler
+
+        
+
+        #endregion
+
+    }
+}
