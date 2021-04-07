ef2688d0d263dcdb3d9a8cbf3edc5a1d6b73194c
adrian_sun <ziqingyi@qq.com>
2021-04-07 17:28:07 +1000

    new CustomHttpModule

---
+++ P37.Course.Web.Core/PipeLine/CustomHttpModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace P37.Course.Web.Core.PipeLine
+{
+    public class CustomHttpModule : IHttpModule
+    {
+        void IHttpModule.Dispose()
+        {
+            Console.WriteLine();
+        }
+
+        void IHttpModule.Init(HttpApplication context)
+        {
+            
+        }
+    }
+}
