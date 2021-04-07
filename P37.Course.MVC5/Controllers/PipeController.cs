98a73ebe7f082efc016c06d97cfb0c7a8882a1bf
adrian_sun <ziqingyi@qq.com>
2021-04-07 13:57:47 +1000

    http process demo

--- P37.Course.MVC5/Controllers/PipeController.cs
+++ P37.Course.MVC5/Controllers/PipeController.cs
@@ -30,4 +30,78 @@ namespace P37.Course.MVC5.Controllers
         #endregion
 
     }
+
+
+    public class HttpProcessDemo
+    {
+        public class HttpApplicationDemo : IHttpHandler
+        {
+            public bool IsReusable => true;
+
+            public event Action BeginRequest;
+            public event Action EndRequest;
+            public event Action PreSomething1Handler;
+            public event Action PostSomething1Handler;
+            public event Action PreSomething2Handler;
+            public event Action PostSomething2Handler;
+            public event Action PreSomething3Handler;
+            public event Action PostSomething3Handler;
+            public event Action PreSomething4Handler;
+            public event Action PostSomething4Handler;
+            public event Action PreSomething5Handler;
+            public event Action PostSomething5Handler;
+            public event Action PreSomething6Handler;
+            public event Action PostSomething6Handler;
+
+            public void ProcessRequest(HttpContext context)
+            {
+                this.BeginRequest?.Invoke();
+
+                this.PreSomething1Handler?.Invoke();
+                Console.WriteLine("Something 1");
+                this.PostSomething1Handler?.Invoke();
+
+                this.PreSomething2Handler?.Invoke();
+                Console.WriteLine("Something 2");
+                this.PostSomething2Handler?.Invoke();
+
+                this.PreSomething3Handler?.Invoke();
+                Console.WriteLine("Something 3");
+                this.PostSomething3Handler?.Invoke();
+
+                this.PreSomething4Handler?.Invoke();
+                Console.WriteLine("Something 4");
+                this.PostSomething4Handler?.Invoke();
+
+                this.PreSomething5Handler?.Invoke();
+                Console.WriteLine("Something 5");
+                this.PostSomething5Handler?.Invoke();
+
+                this.PreSomething6Handler?.Invoke();
+                Console.WriteLine("Something 6");
+                this.PostSomething6Handler?.Invoke();
+
+                this.EndRequest?.Invoke();
+            }
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
