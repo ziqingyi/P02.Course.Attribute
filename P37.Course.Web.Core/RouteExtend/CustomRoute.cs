c8d53923c6685d05996d9ba962811f10915e623d
adrian_sun <ziqingyi@qq.com>
2021-04-15 22:24:13 +1000

    custom Route

---
+++ P37.Course.Web.Core/RouteExtend/CustomRoute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace P37.Course.Web.Core.RouteExtend
+{
+    public class CustomRoute:RouteBase
+    {
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.UserAgent.Contains("Chrome/74.0.3729.169"))
+            {
+                return null;//continue following logic
+            }
+            else
+            {
+                RouteData routeData = new RouteData(this, new MvcRouteHandler());
+                routeData.Values.Add("controller","pipe");
+                routeData.Values.Add("action","refuse");
+                return routeData;// break route mapping
+            }
+
+        }
+
+
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
+        {
+            return null;
+        }
+    }
+}
