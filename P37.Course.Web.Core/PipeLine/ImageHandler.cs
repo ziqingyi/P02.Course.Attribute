cb1dfbcf2c0a55c610878e6d6be5263d37a4db04
adrian_sun <ziqingyi@qq.com>
2021-04-16 11:38:28 +1000

    picture anti-theft chain

---
+++ P37.Course.Web.Core/PipeLine/ImageHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace P37.Course.Web.Core.PipeLine
+{
+    public class ImageHandler:IHttpHandler
+    {
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+        //anti-theft chain, configure in Web.config
+        public void ProcessRequest(HttpContext context)
+        {
+            // if Urlreferrer is empty, means trying to get the image by url link directly, like crawler, then return below.
+            if (context.Request.UrlReferrer == null || context.Request.UrlReferrer.Host == null)
+            {
+                context.Response.ContentType = "image/JPEG";
+                context.Response.WriteFile("/img/noImage.jpg");
+            }
+            else
+            {
+                // if Urlreferrer has server name, show request picture. 
+                if (context.Request.UrlReferrer.Host.Contains("localhost"))
+                {
+                    //get physical path for the image
+                    string fileName = context.Server.MapPath(context.Request.FilePath);
+                    context.Response.ContentType = "image/JPEG";
+                    context.Response.WriteFile(fileName);
+                }
+                else
+                {
+                    context.Response.ContentType = "image/JPEG";
+                    context.Response.WriteFile("/img/noImage.jpg");
+                }
+            }
+        }
+
+        #endregion
+
+
+    }
+}
