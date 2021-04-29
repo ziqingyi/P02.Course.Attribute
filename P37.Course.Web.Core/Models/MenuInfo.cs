c64e339b78cd4a50cf12cd9b1ce15ac2f2663444
adrian_sun <ziqingyi@qq.com>
2021-04-29 17:39:32 +1000

    MenuInfo

---
+++ P37.Course.Web.Core/Models/MenuInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P37.Course.Web.Core.Models
+{
+    public class MenuInfo
+    {
+        public Guid MenuGuid { get; set; }
+        public string MenuName { get; set; }
+        public string Ico { get; set; }
+        public string Url { get; set; }
+        public bool IsLeaf { get; set; }
+        public List<MenuInfo> Submenu { get; set; }
+    }
+}
