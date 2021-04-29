f9fce560e454369e843034955f91e3a5d27fc6e6
adrian_sun <ziqingyi@qq.com>
2021-04-30 08:24:58 +1000

    TreeResultData

---
+++ P37.Course.Web.Core/Models/TreeResultData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P37.Course.Web.Core.Models
+{
+    public class TreeResultData
+    {
+        public Guid id { get; set; }
+        public string title { get; set; }
+        public string href { get; set; }
+  
+        //public bool isChecked { get; set; }
+        public bool disabled { get; set; }
+
+        public List<TreeResultData> children { get; set; }
+
+
+    }
+}
