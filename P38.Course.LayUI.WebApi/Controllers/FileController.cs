16919a89292080068a34fe137ef5f17e3f6a30b7
adrian_sun <ziqingyi@qq.com>
2021-05-08 16:59:46 +1000

    save file api

--- P38.Course.LayUI.WebApi/Controllers/FileController.cs
+++ P38.Course.LayUI.WebApi/Controllers/FileController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Http;
 using P37.Course.Web.Core.Attributes;
 
@@ -27,9 +29,29 @@ namespace P38.Course.LayUI.WebApi.Controllers
                 //get file name
                 var fileName = file.FileName;
 
+                //get extend name
+                int idxStart = fileName.LastIndexOf(".")+1;
+                string extensionName = fileName.Substring(idxStart, fileName.Length - idxStart);
 
+                //get Guid for file name
+                string fileStorageName = Guid.NewGuid().ToString();
 
+                //generate file address
+                string filePath = Path.Combine(HostingEnvironment.MapPath("~/fileUpload"),
+                    $"{fileStorageName}.{extensionName}");
 
+                //save to this address
+                request.Files[0].SaveAs(filePath);
+
+                //return result
+                var result = new
+                {
+                    StorageName = $"{fileStorageName}.{extensionName}", //save file name used for storage
+                    OldFileName = fileName,
+                    IsImage = file.ContentType.Split('/')[0] == "image"
+                };
+                string JsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+                return JsonResult;
             }
             catch (Exception e)
             {
