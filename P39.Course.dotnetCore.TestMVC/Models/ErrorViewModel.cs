32b15a2236a9e9a8c02a4ad1f961e7d3f4709c40
adrian_sun <ziqingyi@qq.com>
2021-05-10 14:03:08 +1000

    Core mvc

---
+++ P39.Course.dotnetCore.TestMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace P39.Course.dotnetCore.TestMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
