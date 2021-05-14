0ae9b73554ef578ac2c876dae58c4a1a47f84b08
adrian_sun <ziqingyi@qq.com>
2021-05-14 19:07:49 +1000

    test default container and provider

--- P34.Course.Business.Service/TestCore/TestServiceB.cs
+++ P34.Course.Business.Service/TestCore/TestServiceB.cs
@@ -11,14 +11,15 @@ namespace P34.Course.Business.Service.TestCore
     public class TestServiceB : ITestServiceB
     {
         private ILogger<TestServiceB> _logger = null;
-        public TestServiceB(ITestServiceA iTestService, ILogger<TestServiceB> logger)
+        public TestServiceB(ITestServiceA iTestServiceA)//, ILogger<TestServiceB> logger)
         {
-            this._logger = logger;
+            Console.WriteLine("initialize TestServiceB with ITestServiceA");
+            //this._logger = logger;
         }
 
         public void Show()
         {
-            this._logger.LogDebug($"This is TestServiceB B123456");
+            //this._logger.LogDebug($"This is TestServiceB B123456");
             Console.WriteLine($"This is TestServiceB B123456");
         }
     }
--- P39.Course.dotnetCore.TestProject/Program.cs
+++ P39.Course.dotnetCore.TestProject/Program.cs
@@ -12,17 +12,40 @@ namespace P39.Course.dotnetCore.TestProject
 
             Console.WriteLine("**************Text IOC ***********************"); ;
             IServiceCollection container = new ServiceCollection();
-            container.AddTransient<ITestServiceA,TestServiceA>();
-            container.AddTransient<ITestServiceB, TestServiceB>();
-            container.AddTransient<ITestServiceC, TestServiceC>();
+            container.AddTransient<ITestServiceA,TestServiceA>();//transient life cycle
+            container.AddSingleton<ITestServiceB, TestServiceB>();
+            container.AddScoped<ITestServiceC, TestServiceC>();//one instance for one scope,eg: in Web, each request.
             container.AddTransient<ITestServiceD, TestServiceD>();
 
             var provider = container.BuildServiceProvider();
 
             ITestServiceA testServiceA = provider.GetService<ITestServiceA>();
+            ITestServiceA testServiceA2 = provider.GetService<ITestServiceA>();
+            Console.WriteLine("for Transient: Equal? "+ object.ReferenceEquals(testServiceA,testServiceA2));
+
             ITestServiceB testServiceB = provider.GetService<ITestServiceB>();
+            ITestServiceB testServiceB2 = provider.GetService<ITestServiceB>();
+            Console.WriteLine("for Singleton: Equal? " + object.ReferenceEquals(testServiceB, testServiceB2));
+
             ITestServiceC testServiceC = provider.GetService<ITestServiceC>();
+            ITestServiceC testServiceC2 = provider.GetService<ITestServiceC>();
+            Console.WriteLine("for Scoped 1 2: Equal? " + object.ReferenceEquals(testServiceC, testServiceC2));
+
+            var scope3 = provider.CreateScope();
+            var scope4 = provider.CreateScope();
+            ITestServiceC testServiceC3 = scope3.ServiceProvider.GetService<ITestServiceC>();
+            ITestServiceC testServiceC4 = scope4.ServiceProvider.GetService<ITestServiceC>();
+            Console.WriteLine("for Scoped 3 4: Equal? " + object.ReferenceEquals(testServiceC3, testServiceC4));//false, different scope
+
+
+
+
             ITestServiceD testServiceD = provider.GetService<ITestServiceD>();
+            ITestServiceD testServiceD2 = provider.GetService<ITestServiceD>();
+            Console.WriteLine("for Scoped: Equal? " + object.ReferenceEquals(testServiceD, testServiceD2));
+
+
+
 
 
             Console.WriteLine("**************Text C# 6************************"); ;
