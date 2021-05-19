140d97d6cc11d90da0e343547a6f7fcebcd2f8d1
adrian_sun <ziqingyi@qq.com>
2021-05-19 10:18:19 +1000

    CustomResultFilterAttribute

---
+++ P39.Course.dotnetCoreLib/Filters/CustomResultFilterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace P39.Course.dotnetCoreLib.Filters
+{
+    public class CustomResultFilterAttribute: Attribute, IResultFilter
+    {
+        private ILogger<CustomResultFilterAttribute> _logger = null;
+
+        public CustomResultFilterAttribute(ILogger<CustomResultFilterAttribute> logger)
+        {
+            this._logger = logger;
+        }
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            //Console.WriteLine("OnResultExecuting Executing!");
+            _logger.LogInformation("OnResultExecuting Executing!");
+        }
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            //Console.WriteLine("OnResultExecuted Executed!");
+            _logger.LogInformation("OnResultExecuted Executed!");
+        }
+
+
+
+
+    }
+}
