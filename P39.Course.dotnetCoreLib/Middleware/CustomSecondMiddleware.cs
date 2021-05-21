70a92ee81f713dc780928a996bee5d49f1ec3b26
adrian_sun <ziqingyi@qq.com>
2021-05-21 13:37:02 +1000

    CustomSecondMiddleware

---
+++ P39.Course.dotnetCoreLib/Middleware/CustomSecondMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace P39.Course.dotnetCoreLib.Middleware
+{
+    public class CustomSecondMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomSecondMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync($"{nameof(CustomMiddleware)} CustomSecondMiddleware <br/>");
+            await _next(context);
+            await context.Response.WriteAsync($"{nameof(CustomMiddleware)} CustomSecondMiddleware <br/>");
+        }
+
+    }
+}
