b412c1b8e6f5fe6555557cfef4b27fcd623ec126
adrian_sun <ziqingyi@qq.com>
2021-05-22 22:19:06 +1000

    CustomAuthorityActionFilterAttribute and core current user

---
+++ P39.Course.dotnetCoreLib/Filters/CustomAuthorityActionFilterAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using P39.Course.dotnetCoreLib.Models;
+
+namespace P39.Course.dotnetCoreLib.Filters
+{
+    public class CustomAuthorityActionFilterAttribute:Attribute, IActionFilter
+    {
+        private ILogger<CustomAuthorityActionFilterAttribute> _logger = null;
+
+        public CustomAuthorityActionFilterAttribute(ILogger<CustomAuthorityActionFilterAttribute> logger)
+        {
+            this._logger = logger;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            this._logger.LogInformation("CustomAuthorityActionFilterAttribute Executing!");
+            string userString = context.HttpContext.Session.GetString("CurrentUser");
+            if (!string.IsNullOrWhiteSpace(userString))
+            {
+                CurrentUserCore currentUser =
+                    Newtonsoft.Json.JsonConvert.DeserializeObject<CurrentUserCore>(userString);
+
+                this._logger.LogInformation($"CustomAuthorityActionFilterAttribute authorize user: {currentUser.Name} log in to the system");
+            }
+            else
+            {
+                context.Result = new RedirectResult("~/Fourth/Login");
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            this._logger.LogInformation("CustomAuthorityActionFilterAttribute Executed!");
+        }
+
+
+    }
+}
---
+++ P39.Course.dotnetCoreLib/Models/CurrentUserCore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P39.Course.dotnetCoreLib.Models
+{
+
+    //this is business model, not database model.
+    [MappingClass("User")]
+    public class CurrentUserCore
+    {
+        public int Id { get; set; }
+        [DisplayName("User Name")]
+        public string Name { get; set; }
+        public string Account { get; set; }
+
+        [DisplayName("User Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        public bool State { get; set; }
+
+        public DateTime LastLoginTime { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public List<Data> Datas { get; set; }
+    }
+    public class Data
+    {
+        public string CompanyID { get; set; }
+        public string Company { get; set; }
+    }
+}
