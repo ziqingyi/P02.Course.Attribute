2641d5881e05e08b63ae885b3c8b69ac62cf6eb0
adrian_sun <ziqingyi@qq.com>
2021-05-24 14:17:46 +1000

    TicketDataFormat

---
+++ P39.Course.dotnetCoreLib/Authentications/TicketDataFormat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace P39.Course.dotnetCoreLib.Authentications
+{
+    public class TicketDataFormat : SecureDataFormat<AuthenticationTicket>// IDataSerializer<AuthenticationTicket>//
+    {
+        public TicketDataFormat(IDataProtector protector)
+            : base(TicketSerializer.Default, protector)
+        {
+        }
+    }
+}
