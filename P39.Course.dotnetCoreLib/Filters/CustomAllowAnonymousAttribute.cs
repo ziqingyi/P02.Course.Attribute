fd3d492ce863c30bbd5b772d18aa58002d1fb096
adrian_sun <ziqingyi@qq.com>
2021-06-07 15:24:52 +1000

    CustomAllowAnonymousAttribute

---
+++ P39.Course.dotnetCoreLib/Filters/CustomAllowAnonymousAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+namespace P39.Course.dotnetCoreLib.Filters
+{
+    public class CustomAllowAnonymousAttribute :Attribute, IAllowAnonymous
+    {
+
+
+    }
+}
