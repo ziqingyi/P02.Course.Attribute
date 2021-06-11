fba483d2f3bbee474670ae39d68a2fd2e42a8fd8
adrian_sun <ziqingyi@qq.com>
2021-06-11 23:09:16 +1000

    update socekt server

--- P40.Course.Socket.Server/SocketServer.cs
+++ P40.Course.Socket.Server/SocketServer.cs
@@ -1,11 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+    Socket sSocket=
 
 namespace P40.Course.Socket.Server
 {
     public class SocketServer
     {
+        public static void Process()
+        {
+            int port = 2018; 
+            string host = "127.0.0.1";
+
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+
+            System.Net.Sockets.Socket sSocket = new System.Net.Sockets.Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            sSocket.Bind(ipe);
+            sSocket.Listen(0);
+
+            //receive a socket
+            System.Net.Sockets.Socket serverSocket = sSocket.Accept();
+            Console.WriteLine("Listening to port: " + port );
+
+            //receive message
+
+            while (true)
+            {
+                string recStr = "";
+                byte[] recByte = new byte[4069];
+                int bytes = serverSocket.Receive(recByte, recByte.Length, 0);
+
+
+
+
+            }
+
+
+
+
+
+        }
 
 
 
@@ -15,8 +53,5 @@ namespace P40.Course.Socket.Server
 
 
 
-
-
-
-    }
+}
 }
