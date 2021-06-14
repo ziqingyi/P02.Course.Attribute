6b5e16b8cfeb78b96073144d7db27683c9eb9ccf
adrian_sun <ziqingyi@qq.com>
2021-06-15 08:50:30 +1000

    SuperSocket server

---
+++ P41.Course.SuperSocket.Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P41.Course.SuperSocket.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("**********************Super Socket ********************");
+
+
+            SuperSocketServer.Init();
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ P41.Course.SuperSocket.Server/SuperSocketServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P41.Course.SuperSocket.Server
+{
+    public class SuperSocketServer
+    {
+
+        public static void Init()
+        {
+            try
+            {
+
+
+
+
+
+
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+            Console.Read();
+
+        }
+
+
+
+
+    }
+}
