0a99c773a14124a81dfb34d6cdca3b617813a245
adrian_sun <ziqingyi@qq.com>
2021-06-15 17:30:14 +1000

    package SocketEngine and init by factory

--- P41.Course.SuperSocket.Server/SuperSocketMain.cs
+++ P41.Course.SuperSocket.Server/SuperSocketMain.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketEngine;
 
 namespace P41.Course.SuperSocket.Server
 {
@@ -13,13 +15,27 @@ namespace P41.Course.SuperSocket.Server
         {
             try
             {
+                IBootstrap bootstrap = BootstrapFactory.CreateBootstrap();
+                if (!bootstrap.Initialize())
+                {
+                    Console.WriteLine("Initialize failed..");
+                    Console.ReadKey();
+                }
 
+                Console.WriteLine("Start the Server");
 
+                foreach (var server in bootstrap.AppServers)
+                {
+                    if (server.State == ServerState.Running)
+                    {
+                        Console.WriteLine($"{server.Name} is running...");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{server.Name} failed...");
+                    }
 
-
-
-
-
+                }
 
 
 
