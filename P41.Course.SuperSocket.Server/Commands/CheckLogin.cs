4bb9603017adc26252524f4a1240f89134a6974b
adrian_sun <ziqingyi@qq.com>
2021-06-16 17:30:39 +1000

    command base for check log in session

---
+++ P41.Course.SuperSocket.Server/Commands/CheckLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P41.Course.SuperSocket.Server.Session;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace P41.Course.SuperSocket.Server.Commands
+{
+    public class CheckLogin : CommandBase<ChatSession, StringRequestInfo>
+    {
+
+        public override void ExecuteCommand(ChatSession session, StringRequestInfo requestInfo)
+        {
+            if (requestInfo.Parameters != null && requestInfo.Parameters.Length == 2)
+            {
+
+
+
+
+            }
+            else
+            {
+                session.Send("Parameter error...");
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
