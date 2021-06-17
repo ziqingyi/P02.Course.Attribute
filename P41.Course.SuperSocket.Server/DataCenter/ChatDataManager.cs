3ab08783089bc1ab939bdfc613be5f6b74294b51
adrian_sun <ziqingyi@qq.com>
2021-06-17 15:05:10 +1000

    chat model and data manager

---
+++ P41.Course.SuperSocket.Server/DataCenter/ChatDataManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P41.Course.SuperSocket.Server.DataCenter
+{
+    public class ChatDataManager
+    {
+        private static Dictionary<string, List<ChatModel>> dictionary = new Dictionary<string, List<ChatModel>>();
+
+        public static void Add(string userId, ChatModel model)
+        {
+            if (dictionary.ContainsKey(userId))
+            {
+                dictionary[userId].Add(model);
+            }
+            else
+            {
+                dictionary[userId] = new List<ChatModel>(){model};
+            }
+        }
+
+        public static void Remove(string userId, string modelId)
+        {
+            if (dictionary.ContainsKey(userId))
+            {
+                dictionary[userId] = dictionary[userId].Where(m => m.Id != modelId).ToList();
+            }
+        }
+
+        public static void SendLogin(string userId, Action<ChatModel> action)
+        {
+            if (dictionary.ContainsKey(userId))
+            {
+                foreach (ChatModel chatModel in dictionary[userId])
+                {
+                    action.Invoke(chatModel);
+                    chatModel.State = 1;
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ P41.Course.SuperSocket.Server/DataCenter/ChatModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P41.Course.SuperSocket.Server.DataCenter
+{
+    public class ChatModel
+    {
+
+        public string Id { get; set; }
+
+        public string FromId { get; set; }
+
+        public string ToId { get; set; }
+
+        public string Message { get; set; }
+    
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// message state  0 undelivered  1 sent   2 received
+        /// </summary>
+        public int State { get; set; }
+
+
+
+
+
+
+    }
+}
