6372fcdf4ae2082200945a45adc67152635da670
adrian_sun <ziqingyi@qq.com>
2021-06-17 13:54:04 +1000

    chat commad with AppServer sessions

---
+++ P41.Course.SuperSocket.Server/Commands/Chat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P41.Course.SuperSocket.Server.Session;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace P41.Course.SuperSocket.Server.Commands
+{
+    public class Chat : CommandBase<ChatSession, StringRequestInfo>
+    {
+        public override void ExecuteCommand(ChatSession session, StringRequestInfo requestInfo)
+        {
+            if (requestInfo.Parameters != null && requestInfo.Parameters.Length == 2)
+            {
+                string toId = requestInfo.Parameters[0];
+                string message = requestInfo.Parameters[1];
+                ChatSession toSession = session.AppServer.GetAllSessions().FirstOrDefault(a => toId.Equals(a.Id));
+
+                string modelId = Guid.NewGuid().ToString();
+
+                if (toSession != null)
+                {
+                    toSession.Send($"Id: {session.Id}  send message to you: {message}  {modelId}");
+
+
+                }
+                else
+                {
+                    
+
+                    session.Send("Message is not delivered");
+                }
+
+
+
+
+
+            }
+
+
+
+        }
+    }
+}
