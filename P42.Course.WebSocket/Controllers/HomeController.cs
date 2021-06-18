bd0e63085b8549697ebd9d15b5f347fa8b153542
adrian_sun <ziqingyi@qq.com>
2021-06-18 17:03:35 +1000

    web socket project, default route

---
+++ P42.Course.WebSocket/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P42.Course.WebSocket.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _userName = "";
+        public ActionResult WebSocket()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        #endregion
+
+
+    }
+}
---
+++ P42.Course.WebSocket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="P42.Course.WebSocket.MvcApplication" Language="C#" %>
