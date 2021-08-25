6d2d430ee4b5e3ba5f164346525a6f45a9a1ec4e
adrian_sun <ziqingyi@qq.com>
2021-08-25 15:58:55 +1000

    system IEnumerator

--- P27.Course.IteratorPattern/Iterator/Iiterator.cs
+++ P27.Course.IteratorPattern/Iterator/Iiterator.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace P27.Course.IteratorPattern.Iterator
 {
+  
     public interface Iiterator<T>
     {
         //current object
@@ -20,4 +21,47 @@ namespace P27.Course.IteratorPattern.Iterator
         void Reset();
 
     }
+
+
+    /*
+     //
+    // Summary:
+    //     Supports a simple iteration over a non-generic collection.
+    [ComVisible(true)]
+    [Guid("496B0ABF-CDEE-11d3-88E8-00902754C43A")]
+    public interface IEnumerator
+    {
+        //
+        // Summary:
+        //     Gets the element in the collection at the current position of the enumerator.
+        //
+        // Returns:
+        //     The element in the collection at the current position of the enumerator.
+        object Current { get; }
+
+        //
+        // Summary:
+        //     Advances the enumerator to the next element of the collection.
+        //
+        // Returns:
+        //     true if the enumerator was successfully advanced to the next element; false if
+        //     the enumerator has passed the end of the collection.
+        //
+        // Exceptions:
+        //   T:System.InvalidOperationException:
+        //     The collection was modified after the enumerator was created.
+        bool MoveNext();
+        //
+        // Summary:
+        //     Sets the enumerator to its initial position, which is before the first element
+        //     in the collection.
+        //
+        // Exceptions:
+        //   T:System.InvalidOperationException:
+        //     The collection was modified after the enumerator was created.
+        void Reset();
+    }
+     *
+     */
+
 }
